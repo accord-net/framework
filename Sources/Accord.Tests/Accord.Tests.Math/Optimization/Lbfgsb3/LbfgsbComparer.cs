ccdde2069c1b8b20256fc72235fac11a4c185df0
MikkelPorse <mikkel.porse@gmail.com>
2014-06-03 14:34:15 +0200

    fixed MachineLearning unittests that used Codification.Translate(params string[])

--- Sources/Accord.Tests/Accord.Tests.Math/Optimization/Lbfgsb3/LbfgsbComparer.cs
+++ Sources/Accord.Tests/Accord.Tests.Math/Optimization/Lbfgsb3/LbfgsbComparer.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using AccordTestsMathCpp2;
 using Accord.Math.Optimization;
+using AccordTestsMathCpp2;
 
 namespace Accord.Tests.Math.Optimization
 {
