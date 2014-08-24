4d0c3348fd986d68a97a7066569b080405afd304
Cesar Souza <cesarsouza@gmail.com>
2014-08-25 00:37:53 +0200

    Adding a new class to load and save models using LibSVM's model definition format.

---
+++ Sources/Accord.Formats/LibSvmModel.cs
@@ -0,0 +1,432 @@
+﻿// Accord Statistics Library
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © César Souza, 2009-2014
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+namespace Accord.IO
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Globalization;
+    using System.IO;
+    using Accord.MachineLearning.VectorMachines;
+    using Accord.MachineLearning.VectorMachines.Learning;
+    using Accord.Statistics.Links;
+
+    /// <summary>
+    ///   Solver types allowed in LibSVM/Liblinear model files.
+    /// </summary>
+    /// 
+    public enum LibSvmSolverType
+    {
+        /// <summary>
+        ///   Unknown solver type.
+        /// </summary>
+        /// 
+        [Description("Unknown")]
+        Unknown = -1,
+
+        /// <summary>
+        ///   L2-regularized logistic regression in the primal (-s 0, L2R_LR).
+        /// </summary>
+        /// 
+        /// <seealso cref="ProbabilisticNewtonMethod"/>
+        /// 
+        [Description("L2R_LR")]
+        L2RegularizedLogisticRegression = 0,
+
+        /// <summary>
+        ///   L2-regularized L2-loss support vector classification
+        ///   in the dual (-s 1, L2R_L2LOSS_SVC_DUAL, the default).
+        /// </summary>
+        /// 
+        /// <seealso cref="LinearCoordinateDescent"/>
+        /// 
+        [Description("L2R_L2LOSS_SVC_DUAL")]
+        L2RegularizedL2LossSvcDual = 1,
+
+        /// <summary>
+        ///   L2-regularized L2-loss support vector classification
+        ///   in the primal (-s 2, L2R_L2LOSS_SVC).
+        /// </summary>
+        /// 
+        /// <seealso cref="ProbabilisticNewtonMethod"/>
+        /// 
+        [Description("L2R_L2LOSS_SVC")]
+        L2RegularizedL2LossSvc = 2,
+
+        /// <summary>
+        ///   L2-regularized L1-loss support vector classification
+        ///   in the dual (-s 3, L2R_L1LOSS_SVC_DUAL).
+        /// </summary>
+        /// 
+        /// <seealso cref="LinearCoordinateDescent"/>
+        /// 
+        [Description("L2R_L1LOSS_SVC_DUAL")]
+        L2RegularizedL1LossSvcDual = 3,
+
+        /// <summary>
+        ///   Support vector classification by 
+        ///   Crammer and Singer (-s 4, MCSVM_CS).
+        /// </summary>
+        /// 
+        [Description("MCSVM_CS")]
+        MulticlassSvmCrammerSinger = 4,
+
+        /// <summary>
+        ///   L1-regularized L2-loss support vector 
+        ///   classification (-s 5, L1R_L2LOSS_SVC).
+        /// </summary>
+        /// 
+        [Description("L1R_L2LOSS_SVC")]
+        L1RegularizedL2LossSvc = 5,
+
+        /// <summary>
+        ///   L1-regularized logistic regression (-s 6, L1R_LR).
+        /// </summary>
+        /// 
+        /// <seealso cref="ProbabilisticCoordinateDescent"/>
+        /// 
+        [Description("L1R_LR")]
+        L1RegularizedLogisticRegression = 6,
+
+        /// <summary>
+        ///   L2-regularized logistic regression in the dual (-s 7, L2R_LR_DUAL).
+        /// </summary>
+        /// 
+        /// <seealso cref="ProbabilisticDualCoordinateDescent"/>
+        /// 
+        [Description("L2R_LR_DUAL")]
+        L2RegularizedLogisticRegressionDual = 7,
+
+        /// <summary>
+        ///   L2-regularized L2-loss support vector regression 
+        ///   in the primal (-s 11, L2R_L2LOSS_SVR).
+        /// </summary>
+        /// 
+        [Description("L2R_L2LOSS_SVR")]
+        L2RegularizedL2LossSvr = 11,
+
+        /// <summary>
+        ///   L2-regularized L2-loss support vector regression 
+        ///   in the dual (-s 12, L2R_L2LOSS_SVR_DUAL).
+        /// </summary>
+        /// 
+        [Description("L2R_L2LOSS_SVR_DUAL")]
+        L2RegularizedL2LossSvrDual = 12,
+
+        /// <summary>
+        ///   L2-regularized L1-loss support vector regression 
+        ///   in the dual (-s 13, L2R_L1LOSS_SVR_DUAL).
+        /// </summary>
+        /// 
+        [Description("L2R_L1LOSS_SVR_DUAL")]
+        L2RegularizedL1LossSvrDual = 13
+    }
+
+    /// <summary>
+    ///   Reads <see cref="SupportVectorMachine">support vector machines</see>
+    ///   created from LibSVM or Liblinear. Not all solver types are supported.
+    /// </summary>
+    /// 
+    public class LibSvmModel
+    {
+
+        private LibSvmSolverType type;
+        private Dictionary<string, LibSvmSolverType> map;
+
+        /// <summary>
+        ///   Gets or sets the solver type used to create the model.
+        /// </summary>
+        /// 
+        public LibSvmSolverType Solver
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        /// <summary>
+        ///   Gets or sets the number of classes that
+        ///   this classification model can handle.
+        /// </summary>
+        /// 
+        public int Classes { get; set; }
+
+        /// <summary>
+        ///   Gets or sets whether an initial double value should
+        ///   be appended in the beginning of every feature vector.
+        ///   If set to a negative number, this functionality is
+        ///   disabled. Default is 0.
+        /// </summary>
+        /// 
+        public double Bias { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the number of dimensions (features) 
+        ///   the classification or regression model can handle.
+        /// </summary>
+        /// 
+        public int Dimension { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the class label for each class
+        ///   this classification model expects to handle.
+        /// </summary>
+        /// 
+        public int[] Labels { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the vector of linear weights used
+        ///   by this model, if it is a compact model. If this
+        ///   is not a compact model, this will be set to <c>null</c>.
+        /// </summary>
+        /// 
+        /// <seealso cref="SupportVectorMachine.IsCompact"/>
+        /// 
+        public double[] Weights { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the set of support vectors used
+        ///   by this model. If the model is compact, this
+        ///   will be set to <c>null</c>.
+        /// </summary>
+        /// 
+        /// <seealso cref="SupportVectorMachine.IsCompact"/>
+        /// 
+        public double[][] Vectors { get; set; }
+
+
+        /// <summary>
+        ///   Creates a new <see cref="LibSvmModel"/> object.
+        /// </summary>
+        /// 
+        public LibSvmModel()
+        {
+            map = new Dictionary<string, LibSvmSolverType>();
+            foreach (LibSvmSolverType v in Enum.GetValues(typeof(LibSvmSolverType)))
+                map.Add(v.GetDescription().ToUpperInvariant(), v);
+        }
+
+        /// <summary>
+        ///   Creates a <see cref="SupportVectorMachine"/> that
+        ///   attends the requisites specified in this model.
+        /// </summary>
+        /// 
+        /// <returns>A <see cref="SupportVectorMachine"/> that represents this model.</returns>
+        /// 
+        public SupportVectorMachine CreateMachine()
+        {
+            switch (type)
+            {
+                case LibSvmSolverType.Unknown:
+                    break;
+
+                case LibSvmSolverType.L2RegularizedLogisticRegression:
+                case LibSvmSolverType.L1RegularizedLogisticRegression:
+                case LibSvmSolverType.L2RegularizedLogisticRegressionDual:
+                    {
+                        var svm = SupportVectorMachine.FromWeights(Weights);
+                        svm.Link = new LogLinkFunction();
+                        return svm;
+                    }
+                case LibSvmSolverType.L2RegularizedL2LossSvc:
+                case LibSvmSolverType.L2RegularizedL1LossSvcDual:
+                case LibSvmSolverType.L2RegularizedL2LossSvcDual:
+                case LibSvmSolverType.L1RegularizedL2LossSvc:
+                case LibSvmSolverType.L2RegularizedL2LossSvr:
+                case LibSvmSolverType.L2RegularizedL2LossSvrDual:
+                case LibSvmSolverType.L2RegularizedL1LossSvrDual:
+                    {
+                        return SupportVectorMachine.FromWeights(Weights);
+                    }
+            }
+
+            throw new NotSupportedException("This solver type is unknown or not supported.");
+        }
+
+        /// <summary>
+        ///   Creates a <see cref="ISupportVectorMachineLearning"> support
+        ///   vector machine learning algorithm</see> that attends the 
+        ///   requisites specified in this model.
+        /// </summary>
+        /// 
+        /// <returns>
+        ///   A <see cref="ISupportVectorMachineLearning"/> that represents this model.
+        /// </returns>
+        /// 
+        public ISupportVectorMachineLearning CreateAlgorithm(double[][] inputs, int[] outputs)
+        {
+            var machine = CreateMachine();
+
+            switch (type)
+            {
+                case LibSvmSolverType.L2RegularizedLogisticRegression:
+                    return new ProbabilisticNewtonMethod(machine, inputs, outputs);
+
+                case LibSvmSolverType.L1RegularizedLogisticRegression:
+                    return new ProbabilisticCoordinateDescent(machine, inputs, outputs);
+
+                case LibSvmSolverType.L2RegularizedLogisticRegressionDual:
+                    return new ProbabilisticDualCoordinateDescent(machine, inputs, outputs);
+
+                case LibSvmSolverType.L2RegularizedL2LossSvc:
+                    return new LinearNewtonMethod(machine, inputs, outputs);
+
+                case LibSvmSolverType.L2RegularizedL1LossSvcDual:
+                    return new LinearCoordinateDescent(machine, inputs, outputs) { Loss = Loss.L1 };
+
+                case LibSvmSolverType.L2RegularizedL2LossSvcDual:
+                    return new LinearCoordinateDescent(machine, inputs, outputs) { Loss = Loss.L2 };
+
+                case LibSvmSolverType.L1RegularizedL2LossSvc:
+                case LibSvmSolverType.L2RegularizedL2LossSvr:
+                case LibSvmSolverType.L2RegularizedL2LossSvrDual:
+                case LibSvmSolverType.L2RegularizedL1LossSvrDual:
+                    break;
+            }
+
+            throw new NotSupportedException("This solver type is unknown or not supported.");
+        }
+
+
+        /// <summary>
+        ///   Saves this model to disk using LibSVM's model format.
+        /// </summary>
+        /// 
+        /// <param name="path">The path where the file should be written.</param>
+        /// 
+        public void Save(string path)
+        {
+            using (var stream = new FileStream(path, FileMode.Create, FileAccess.Write))
+                Save(stream);
+        }
+
+        /// <summary>
+        ///   Saves this model to disk using LibSVM's model format.
+        /// </summary>
+        /// 
+        /// <param name="stream">The stream where the file should be written.</param>
+        /// 
+        public void Save(Stream stream)
+        {
+            StreamWriter writer = new StreamWriter(stream);
+
+            writer.WriteLine("solver_type " + Solver.GetDescription().ToUpperInvariant());
+            writer.WriteLine("nr_class " + Classes);
+
+            writer.Write("label");
+            for (int i = 0; i < Labels.Length; i++)
+                writer.Write(" " + Labels[i]);
+            writer.WriteLine();
+
+            writer.WriteLine("nr_feature " + Dimension);
+            writer.WriteLine("bias " + Bias.ToString("G17", CultureInfo.InvariantCulture));
+            writer.WriteLine("w");
+
+            for (int i = 0; i < Weights.Length; i++)
+                writer.WriteLine(Weights[i].ToString("G17", CultureInfo.InvariantCulture) + " ");
+
+            writer.Flush();
+        }
+
+        /// <summary>
+        ///   Loads a model specified using LibSVM's model format from disk.
+        /// </summary>
+        /// 
+        /// <param name="path">The file path from where the model should be loaded.</param>
+        /// 
+        /// <returns>The <see cref="LibSvmModel"/> stored on <paramref name="path"/>.</returns>
+        /// 
+        public static LibSvmModel Load(string path)
+        {
+            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                return Load(stream);
+        }
+
+        /// <summary>
+        ///   Loads a model specified using LibSVM's model format from a stream.
+        /// </summary>
+        /// 
+        /// <param name="stream">The stream from where the model should be loaded.</param>
+        /// 
+        /// <returns>The <see cref="LibSvmModel"/> stored on <paramref name="stream"/>.</returns>
+        /// 
+        public static LibSvmModel Load(Stream stream)
+        {
+            LibSvmModel model = new LibSvmModel();
+
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] words = line.Split(' ');
+
+                    if (words[0] == "solver_type")
+                    {
+                        string key = words[1].ToUpperInvariant();
+                        if (!model.map.TryGetValue(key, out model.type))
+                            model.type = LibSvmSolverType.Unknown;
+                    }
+
+                    else if (words[0] == "nr_class")
+                        model.Classes = Int32.Parse(words[1]);
+
+                    else if (words[0] == "nr_feature")
+                        model.Dimension = Int32.Parse(words[1]);
+
+                    else if (words[0] == "bias")
+                        model.Bias = Double.Parse(words[1], CultureInfo.InvariantCulture);
+
+                    else if (words[0] == "w")
+                        break;
+
+                    else if (words[0] == "label")
+                    {
+                        model.Labels = new int[words.Length - 1];
+                        for (int i = 1; i < words.Length; i++)
+                            model.Labels[i - 1] = Int32.Parse(words[i]);
+                    }
+                    else
+                    {
+                        System.Diagnostics.Trace.WriteLine("Unknown field: " + words[0]);
+                    }
+                }
+
+                List<double> weights = new List<double>();
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    weights.Add(Double.Parse(line, CultureInfo.InvariantCulture));
+                }
+
+                model.Weights = weights.ToArray();
+            }
+
+            return model;
+        }
+
+
+
+
+    }
+}
---
+++ Unit Tests/Accord.Tests.IO/LibSvmModelTest.cs
@@ -0,0 +1,208 @@
+﻿// Accord Unit Tests
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © César Souza, 2009-2014
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+namespace Accord.Tests.IO
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using Accord.IO;
+    using Accord.Tests.IO.Properties;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Accord.Math;
+    using System.Globalization;
+
+    [TestClass()]
+    public class LibSvmModelTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+
+
+        [TestMethod()]
+        public void ReadLinearMachineTest()
+        {
+            MemoryStream file = new MemoryStream(
+              Encoding.Default.GetBytes(Accord.Tests.IO.Properties.Resources.L1R_LR_a9a2_txt));
+
+            LibSvmModel reader = LibSvmModel.Load(file);
+
+            Assert.AreEqual(-1, reader.Bias);
+            Assert.AreEqual(2, reader.Classes);
+            Assert.AreEqual(123, reader.Dimension);
+            Assert.AreEqual(2, reader.Labels.Length);
+            Assert.AreEqual(+1, reader.Labels[0]);
+            Assert.AreEqual(-1, reader.Labels[1]);
+            Assert.AreEqual(LibSvmSolverType.L1RegularizedLogisticRegression, reader.Solver);
+            Assert.AreEqual(null, reader.Vectors);
+            Assert.AreEqual(123, reader.Weights.Length);
+
+
+
+            for (int i = 0; i < a9a_weights.Length; i++)
+                Assert.AreEqual(a9a_weights[i], reader.Weights[i]);
+
+
+            var machine = reader.CreateMachine();
+
+            Assert.IsNull(machine.SupportVectors);
+            Assert.IsNotNull(machine.Link);
+            Assert.AreEqual(machine.Threshold, a9a_weights[0]);
+
+            for (int i = 0; i < machine.Weights.Length; i++)
+                Assert.AreEqual(machine.Weights[i], a9a_weights[i + 1]);
+        }
+
+        [TestMethod()]
+        public void WriteLinearMachineTest()
+        {
+            MemoryStream destination = new MemoryStream();
+
+            var model = new LibSvmModel()
+            {
+                 Bias = -1,
+                 Classes = 2, 
+                 Dimension = 123,
+                 Labels = new[] { +1, -1 },
+                 Solver = LibSvmSolverType.L1RegularizedLogisticRegression,
+                 Weights = a9a_weights
+            };
+
+            model.Save(destination);
+
+            destination.Seek(0, SeekOrigin.Begin);
+            TextReader textReader = new StreamReader(destination);
+            string[] actual = textReader.ReadToEnd()
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            string[] expected = Resources.L1R_LR_a9a2_txt
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            Assert.AreEqual(expected.Length, actual.Length);
+
+            for (int i = 0; i < 7; i++)
+                Assert.AreEqual(expected[i], actual[i]);
+
+            for (int i = 6; i < expected.Length; i++)
+            {
+                if (expected[i] == actual[i])
+                    continue;
+
+                double a = Double.Parse(expected[i], CultureInfo.InvariantCulture);
+                double b = Double.Parse(actual[i], CultureInfo.InvariantCulture);
+
+                Assert.AreEqual(a, b);
+            }
+
+            Assert.AreEqual(expected[expected.Length - 1], String.Empty);
+        }
+
+        [TestMethod()]
+        public void WriteLinearMachineTest_ExactCopy()
+        {
+            MemoryStream file = new MemoryStream(
+              Encoding.Default.GetBytes(Resources.L1R_LR_a9a2_txt));
+
+            LibSvmModel reader = LibSvmModel.Load(file);
+
+            MemoryStream destination = new MemoryStream();
+            reader.Save(destination);
+
+            destination.Seek(0, SeekOrigin.Begin);
+
+            TextReader textReader = new StreamReader(destination);
+            string[] actual = textReader.ReadToEnd()
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            string[] expected = Resources.L1R_LR_a9a2_txt
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            Assert.AreEqual(expected.Length, actual.Length);
+
+            for (int i = 0; i < 7; i++)
+                Assert.AreEqual(expected[i], actual[i]);
+
+            for (int i = 6; i < expected.Length; i++)
+            {
+                if (expected[i] == actual[i])
+                    continue;
+
+                double a = Double.Parse(expected[i], CultureInfo.InvariantCulture);
+                double b = Double.Parse(actual[i], CultureInfo.InvariantCulture);
+
+                Assert.AreEqual(a, b);
+            }
+
+            Assert.AreEqual(expected[expected.Length - 1], String.Empty);
+        }
+
+
+        private static double[] a9a_weights = 
+        {
+            -1.582078049646382,   -0.642353059177759,   -0.03749159485897963,  0.2771411674145598,
+             0.2653433548837567,   0.3415826259468487,  -0.05175901290550707,  0.664475778321269,
+             0.7785762379454263,   0.09938683894685095, -0.01638170625676196, -1.966943713947588,
+             0,                   -0.1943487933572316,   0,                   -0.01427068979115089,
+             0.03436343933761782,  0.04405623496590667, -0.2671859968627904,  -0.01805747891909384,
+             0.5658635598535398,  -0.05370627691645281,  0.6313660889689086,   0.0203659899281737,
+             0,                   -0.1356879782597286,  -0.05013455680484838,  0.8274477288905491,
+             0,                    0,                    0.3329462841850907,   0.7437508445692492,
+            -0.07194377091195173, -1.926282111405892,   -1.494061719727004,   -0.3166280869327708,
+             0,                    0.1427422938386556,   1.007621381801709,    1.789552587610372,
+            -0.4475053816996695,  -0.577595376635344,   -0.5775327047371792,   0.07589587177513718,
+            -0.2633062199626867,   2.16187459176026,     0.7074073117375321,   0.1048275739725781,
+            -0.7075406813675604,   0.3929609161252481,   0.8013002964263505,    0.6108089768685096,
+            -0.6786424810841956,  -0.3491153276395321,   0.09966883995979201, -0.8531071757633746,
+             0,                   -1.398866235553087,    0.7296056533697333,  -0.4887607820122212,
+             1.078440711552346,   -0.7974653657893828,  -0.365703925070073,    0.227254295285581,
+            -0.6760816771256906,   0,                    0.1009172420096765,   0.1545114041709269,
+            -0.3633029234077111,  -0.3484744324185876,  -0.02191163808743305, -0.8402401543317437,
+             0,                   -1.624966833470328,    0,                   -1.122080608688373,
+             0,                   -1.220098499608685,   -0.4182725480858192,  -0.183241852284002,
+             0.1221263566926127,   0.282442600832818,    0.3074189050166497,   0.7959947049068736,
+             0.179689226126266,   -0.5067405511765133,   0.592363376246412,    0.6766998296161411,
+            -0.991029010354986,   -0.2349170170278676,   0.5804018208787766,  -0.93788799387005,
+            -0.5434195848637735,  -0.5901649721443204,   0.3803698957580355,   0.2590827574224139,
+             0,                    0.7183509687151004,   0.8098991683873735,   0.08993105400479942,
+            -0.07476049969988381, -0.9283947988462313,  -0.3739799928196428,   0.03171888330308115,
+             1.197937572543021,    0.7364578149257212,  -1.349880912920405,   -0.131225067370858,
+             0,                    0.1703791693215565,   0.2216059824256973,  -2.031437061283596,
+             0,                   -0.4399798222194601,  -0.0728251038840361,   0,
+             0,                    0.480264506314448,   -0.3699822088077383,  -0.1089542718979226,
+            -1.247016957015914,    0,                    0, 
+        };
+    }
+}
