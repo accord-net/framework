4a7a9a7b0d4c5bc3c8255d6209207b87c831a3fe
Cesar Souza <cesarsouza@gmail.com>
2014-09-07 22:01:20 +0200

    Adding Red-Black trees.

---
+++ Sources/Accord.Core/Collections/RedBlackTree`2.cs
@@ -0,0 +1,95 @@
+﻿// Accord Core Library
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © Steven G. Johnson, 2008
+// stevenj@alum.mit.edu
+//
+// Copyright © César Souza, 2009-2014
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+//
+// The source code presented in this file has been adapted from the
+// Red Black tree implementation presented in the NLopt Numerical 
+// Optimization Library. Original license details are given below.
+//
+//    Copyright (c) 2007-2011 Massachusetts Institute of Technology
+//
+//    Permission is hereby granted, free of charge, to any person obtaining
+//    a copy of this software and associated documentation files (the
+//    "Software"), to deal in the Software without restriction, including
+//    without limitation the rights to use, copy, modify, merge, publish,
+//    distribute, sublicense, and/or sell copies of the Software, and to
+//    permit persons to whom the Software is furnished to do so, subject to
+//    the following conditions:
+// 
+//    The above copyright notice and this permission notice shall be
+//    included in all copies or substantial portions of the Software.
+// 
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+//
+
+namespace Accord.Collections
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///   Red-black tree specialized for key-based value retrieval.
+    /// </summary>
+    /// 
+    /// <remarks>
+    ///   See <see cref="RedBlackTree{T}"/>.
+    /// </remarks>
+    /// 
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
+    /// 
+    [Serializable]
+    public class RedBlackTree<TKey, TValue> : RedBlackTree<KeyValuePair<TKey, TValue>>
+    {
+
+        /// <summary>
+        ///   Constructs a new <see cref="RedBlackTree&lt;T&gt;"/> using the default
+        ///   <see cref="IComparer{T}"/> for the key type <typeparamref name="TKey"/>.
+        /// </summary>
+        /// 
+        public RedBlackTree()
+            : base(KeyValuePairComparer<TKey, TValue>.Default)
+        {
+        }
+
+        /// <summary>
+        ///   Constructs a new <see cref="RedBlackTree&lt;T&gt;"/> using 
+        ///   the provided <see cref="IComparer{T}"/> implementation.
+        /// </summary>
+        /// 
+        /// <param name="comparer">The key-pair comparer to be used.</param>
+        /// 
+        public RedBlackTree(IComparer<KeyValuePair<TKey, TValue>> comparer)
+            : base(comparer)
+        {
+        }
+
+    }
+}
---
+++ Unit Tests/Accord.Tests.Math/Collections/RedBlackTreeTest.cs
@@ -0,0 +1,257 @@
+﻿// Accord Unit Tests
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © César Souza, 2009-2014
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+namespace Accord.Tests.Math
+{
+    using System;
+    using Accord.Collections;
+    using Accord.Math;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    [TestClass()]
+    public class RedBlackTreeTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod()]
+        public void BaseTest()
+        {
+            run(10);
+        }
+
+        [TestMethod()]
+        public void ExtensiveTest()
+        {
+            for (int i = 1; i < 10; i ++)
+                run(i);
+
+            for (int i = 10; i < 1500; i += 47)
+                run(i);
+
+            run(5000);
+        }
+
+
+        [TestMethod()]
+        public void DuplicateTest()
+        {
+            RedBlackTree<int> t = new RedBlackTree<int>();
+            t.Add(1);
+            t.Add(2);
+            t.Add(1);
+
+            int[] values = (t as ICollection<int>).ToArray();
+            Assert.Fail();
+
+            var node1 = t.Remove(1);
+            var node2 = t.Remove(1);
+
+        }
+
+       
+
+        private static void run(int n)
+        {
+            var rand = Accord.Math.Tools.Random;
+
+            RedBlackTree<int> t = new RedBlackTree<int>();
+
+            // Create a vector of random numbers
+            int[] k = new int[n];
+            for (int i = 0; i < k.Length; i++)
+                k[i] = rand.Next(k.Length);
+
+            int[] sorted = (int[])k.Clone();
+            Array.Sort(sorted);
+
+            // Populate the tree with numbers
+            for (int i = 0; i < k.Length; i++)
+            {
+                var node = t.Add(k[i]);
+
+                Assert.IsNotNull(node);
+                Assert.AreEqual(k[i], node.Value);
+
+                Assert.IsTrue(t.check());
+            }
+
+            Assert.AreEqual(k.Length, t.Count);
+
+
+            // Check that all elements are in the tree
+            for (int i = 0; i < k.Length; ++i)
+            {
+                var node = t.Find(k[i]);
+
+                Assert.IsNotNull(node);
+                Assert.AreEqual(k[i], node.Value);
+
+                Assert.IsTrue(t.Contains(k[i]));
+                Assert.IsTrue(t.Contains(node));
+            }
+
+            // Enumerate the values (must be in order)
+            int arrayIndex = 0;
+            foreach (var node in t)
+                Assert.AreEqual(sorted[arrayIndex++], node.Value);
+
+
+
+            // Start from min and go navigating up to max
+            var min = t.Min();
+            Assert.IsNotNull(min);
+            Assert.AreEqual(k.Min(), min.Value);
+
+            for (int i = 0; i < k.Length; i++)
+            {
+                Assert.IsNotNull(min);
+                min = t.GetNextNode(min);
+            }
+            Assert.IsNull(min); // the last should be null.
+
+            // Start from max and go navigating down to min
+            var max = t.Max();
+            Assert.AreEqual(k.Max(), max.Value);
+            for (int i = 0; i < k.Length; i++)
+            {
+                Assert.IsNotNull(max);
+                max = t.GetPreviousNode(max);
+            }
+            Assert.IsNull(max); // the last should be null.
+
+
+            // Exercise the tree
+            for (int M = k.Length; M > 0; M--)
+            {
+                int knew = rand.Next(k.Length); // random new key 
+                int j = rand.Next(M); // random original key to replace 
+                int i;
+
+                for (i = 0; i < k.Length; i++)
+                    if (k[i] >= 0)
+                        if (j-- == 0)
+                            break;
+
+
+                if (i >= k.Length)
+                    Assert.Fail();
+
+                int kd = k[i];
+
+                var node = t.Find(kd);
+                Assert.IsNotNull(node);
+
+                node.Value = knew;
+
+                Assert.IsNotNull(t.Resort(node));
+                Assert.IsTrue(t.check());
+
+                k[i] = -1 - knew;
+
+                Assert.AreEqual(k.Length, t.Count);
+            }
+
+            for (int i = 0; i < k.Length; i++)
+                k[i] = -1 - k[i]; // undo negation above
+
+
+            
+
+            // check the localization functions
+            for (int i = 0; i < k.Length; i++)
+            {
+                int kd = (int)(0.01 * (rand.Next() % (k.Length * 150) - k.Length * 25));
+
+                var le = t.FindLessThanOrEqualTo(kd);
+                var gt = t.FindGreaterThan(kd);
+
+                var node = t.Min();
+
+                double lek = le != null ? le.Value : int.MinValue;
+                double gtk = gt != null ? gt.Value : int.MaxValue;
+
+
+                if (node.Value > kd)
+                {
+                    Assert.IsNull(le);
+                    Assert.IsNotNull(gt);
+                    Assert.AreEqual(gt, node);
+                }
+                else
+                {
+                    var succ = node;
+                    do
+                    {
+                        node = succ;
+                        succ = t.GetNextNode(node);
+                    } while (succ != null && succ.Value <= kd);
+
+                    Assert.AreEqual(node, le);
+                    Assert.AreEqual(succ, gt);
+                }
+            }
+
+            // Remove elements from the tree
+            for (int M = k.Length; M > 0; M--)
+            {
+                int j = rand.Next() % M;
+                int i;
+                for (i = 0; i < k.Length; i++)
+                    if (k[i] >= 0)
+                        if (j-- == 0)
+                            break;
+
+                if (i >= k.Length)
+                    Assert.Fail();
+
+                int kd = k[i];
+
+                var node = t.Find(kd);
+                Assert.IsNotNull(node);
+
+                node = t.Remove(node);
+                Assert.IsTrue(t.check());
+
+                k[i] = -1 - k[i];
+            }
+
+            // The tree should be empty
+            Assert.AreEqual(0, t.Count);
+        }
+    }
+}
