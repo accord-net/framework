c40063415570c930c9d3398411fd0234cf4b8fd5
Cesar Souza <cesarsouza@gmail.com>
2014-09-23 19:26:57 +0200

    Adding Rectified Linear activation functions.

---
+++ Sources/Accord.Neuro/Activation Functions/IdentityFunction.cs
@@ -0,0 +1,105 @@
+﻿// Accord Neural Net Library
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © César Souza, 2009-2014
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+namespace Accord.Neuro
+{
+    using System;
+    using AForge;
+    using AForge.Neuro;
+
+    /// <summary>
+    ///   Identity activation function.
+    /// </summary>
+    /// 
+    /// <remarks>
+    ///   The identity activation function is given by <c>f(x) = x</c>,
+    ///   meaning it simply repasses the neuronal summation output to
+    ///   further neurons untouched. 
+    /// </remarks>
+    /// 
+    [Serializable]
+    public class IdentityFunction : IActivationFunction
+    {
+
+        /// <summary>
+        ///   Creates a new identity activation function.
+        /// </summary>
+        /// 
+        public IdentityFunction()
+        {
+        }
+
+        /// <summary>
+        ///   Calculates function value.
+        /// </summary>
+        ///
+        /// <param name="x">Function input value.</param>
+        /// 
+        /// <returns>Function output value, <i>f(x)</i>.</returns>
+        ///
+        /// <remarks>The method calculates function value at point <paramref name="x"/>.</remarks>
+        ///
+        public double Function(double x)
+        {
+            return x;
+        }
+
+        /// <summary>
+        ///   Calculates function derivative.
+        /// </summary>
+        /// 
+        /// <param name="x">Function input value.</param>
+        /// 
+        /// <returns>Function derivative, <i>f'(x)</i>.</returns>
+        /// 
+        /// <remarks>The method calculates function derivative at point <paramref name="x"/>.</remarks>
+        ///
+        public double Derivative(double x)
+        {
+            return 1;
+        }
+
+        /// <summary>
+        /// Calculates function derivative.
+        /// </summary>
+        /// 
+        /// <param name="y">Function output value - the value, which was obtained
+        /// with the help of <see cref="Function"/> method.</param>
+        /// 
+        /// <returns>Function derivative, <i>f'(x)</i>.</returns>
+        /// 
+        /// <remarks><para>The method calculates the same derivative value as the
+        /// <see cref="Derivative"/> method, but it takes not the input <b>x</b> value
+        /// itself, but the function value, which was calculated previously with
+        /// the help of <see cref="Function"/> method.</para>
+        /// 
+        /// <para><note>Some applications require as function value, as derivative value,
+        /// so they can save the amount of calculations using this method to calculate derivative.</note></para>
+        /// </remarks>
+        /// 
+        public double Derivative2(double y)
+        {
+            return 1;
+        }
+
+    }
+}
