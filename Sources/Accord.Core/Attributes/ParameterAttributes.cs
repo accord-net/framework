739b6a5c3b0605c02831a83be0a4fd32a8d16268
Cesar Souza <cesarsouza@gmail.com>
2014-11-10 21:18:20 +0100

    Correcting attribute namespace.

--- Sources/Accord.Core/Attributes/ParameterAttributes.cs
+++ Sources/Accord.Core/Attributes/ParameterAttributes.cs
@@ -20,7 +20,7 @@
 //    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
-namespace Accord.Math
+namespace Accord
 {
     using System.ComponentModel.DataAnnotations;
     using System;
