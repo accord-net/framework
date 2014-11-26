74518a9e2c251dfb9533caf0815e67dcbe691de3
Cesar Souza <cesarsouza@gmail.com>
2014-11-27 00:29:26 +0100

    GH-49: Expanding documentation and adding more argument checks in Kumaraswamy distribution.

--- Sources/Accord.Statistics/Distributions/Univariate/Continuous/KumaraswamyDistribution.cs
+++ Sources/Accord.Statistics/Distributions/Univariate/Continuous/KumaraswamyDistribution.cs
@@ -26,6 +26,7 @@ namespace Accord.Statistics.Distributions.Univariate.Continuous
 
     using System;
     using Accord.Math;
+    using AForge;
 
     /// <summary>
     ///   Kumaraswamy distribution.
@@ -60,16 +61,25 @@ namespace Accord.Statistics.Distributions.Univariate.Continuous
     ///   of an Kumaraswamy distribution given its two non-negative shape parameters: </para>
     ///   
     /// <code>
-    ///     var kumaraswamyDistribution = new KumaraswamyDistribution(0.2d, 1.2d);
-    ///     double mean = kumaraswamyDistribution.Mean; //0.1258821823337952
-    ///     double variance = kumaraswamyDistribution.Variance; //0.045500725605275683
-    ///     double median = kumaraswamyDistribution.Median; //0.016262209853672775
-    ///     double mode = kumaraswamyDistribution.Mode; //NaN  
-    ///     
-    ///     double pdf = kumaraswamyDistribution.ProbabilityDensityFunction(0.3); //0.46195081771596241
-    ///     double cdf = kumaraswamyDistribution.CumulativeHazardFunction(0.3); //1.8501524192880519
-    ///     string tostr = kumaraswamyDistribution.ToString(CultureInfo.InvariantCulture); // Kumaraswamy(x; a = 0.2, b = 1.2)
-    ///     
+    /// // Create a new Kumaraswamy distribution with shape (4,2)
+    /// var kumaraswamy = new KumaraswamyDistribution(a: 4, b: 2);
+    /// 
+    /// double mean = kumaraswamy.Mean;     // 0.71111111111111114
+    /// double median = kumaraswamy.Median; // 0.73566031573423674
+    /// double mode = kumaraswamy.Mode;     // 0.80910671157022118
+    /// double var = kumaraswamy.Variance;  // 0.027654320987654302
+    /// 
+    /// double cdf = kumaraswamy.DistributionFunction(x: 0.4);           // 0.050544639999999919
+    /// double pdf = kumaraswamy.ProbabilityDensityFunction(x: 0.4);     // 0.49889280000000014
+    /// double lpdf = kumaraswamy.LogProbabilityDensityFunction(x: 0.4); // -0.69536403596913343
+    /// 
+    /// double ccdf = kumaraswamy.ComplementaryDistributionFunction(x: 0.4); // 0.94945536000000008
+    /// double icdf = kumaraswamy.InverseDistributionFunction(p: cdf);       // 0.40000011480618253
+    /// 
+    /// double hf = kumaraswamy.HazardFunction(x: 0.4);            // 0.52545155993431869
+    /// double chf = kumaraswamy.CumulativeHazardFunction(x: 0.4); // 0.051866764053008864
+    /// 
+    /// string str = kumaraswamy.ToString(CultureInfo.InvariantCulture); // Kumaraswamy(x; a = 4, b = 2)
     /// </code>
     /// </example>
     /// 
@@ -79,87 +89,186 @@ namespace Accord.Statistics.Distributions.Univariate.Continuous
         double a;
         double b;
 
+        /// <summary>
+        ///   Constructs a new Kumaraswamy's double bounded distribution with 
+        ///   the given two non-negative shape parameters <c>a</c> and <c>b</c>.
+        /// </summary>
+        /// 
+        /// <param name="a">The distribution's non-negative shape parameter a.</param>
+        /// <param name="a">The distribution's non-negative shape parameter b.</param>
+        /// 
         public KumaraswamyDistribution([Positive] double a, [Positive] double b)
         {
-            if (a <= 0.0d || b < 0.0d) {
-                throw new ArgumentOutOfRangeException(string.Format("a and b must be positive: a={0}, b={1}", a, b));
-            }
+            if (a <= 0)
+                throw new ArgumentOutOfRangeException("The shape parameter a must be positive.");
+
+            if (b < 0)
+                throw new ArgumentOutOfRangeException("The shape parameter b must be positive.");
+
             this.a = a;
             this.b = b;
         }
 
+        /// <summary>
+        ///   Gets the mean for this distribution.
+        /// </summary>
+        /// 
+        /// <value>
+        ///   The distribution's mean value.
+        /// </value>
+        /// 
         public override double Mean
         {
-            get 
-            {  
-                return   ( b * Gamma.Function( 1.0d + ( 1.0d / a ) ) * Gamma.Function( b ) ) /
-                            ( Gamma.Function( 1.0d + ( 1.0d / a ) + b ) );                        
+            get
+            {
+                double num = b * Gamma.Function(1 + (1 / a)) * Gamma.Function(b);
+                double den = Gamma.Function(1 + (1 / a) + b);
+
+                return num / den;
             }
         }
 
+        /// <summary>
+        ///   Gets the variance for this distribution.
+        /// </summary>
+        /// 
+        /// <value>
+        ///   The distribution's variance.
+        /// </value>
+        /// 
         public override double Variance
         {
-            get 
+            get
             {
-                return (MomentGeneratingFunction(2, a, b) - Math.Pow(MomentGeneratingFunction(1, a, b), 2.0d));
+                double alpha = momentGeneratingFunction(2, a, b);
+                double beta = Math.Pow(momentGeneratingFunction(1, a, b), 2);
+                return alpha - beta;
             }
         }
 
-        private double MomentGeneratingFunction(int n, double a, double b) 
+        private double momentGeneratingFunction(int n, double a, double b)
         {
-            return ( b * Beta.Function( 1.0d + ((double) n) / a, b ));
+            return (b * Beta.Function(1.0d + ((double)n) / a, b));
         }
 
-
-
+        /// <summary>
+        ///   Gets the median for this distribution.
+        /// </summary>
+        /// 
+        /// <value>
+        ///   The distribution's median value.
+        /// </value>
+        /// 
         public override double Median
         {
             get
             {
-                return Math.Pow( 1.0d - Math.Pow( 2.0d, -1.0d / b), 1.0d / a );
+                return Math.Pow(1 - Math.Pow(2, -1 / b), 1 / a);
             }
         }
 
+        /// <summary>
+        ///   Gets the mode for this distribution.
+        /// </summary>
+        /// 
+        /// <value>
+        ///   The distribution's mode value.
+        /// </value>
+        /// 
         public override double Mode
         {
             get
             {
 
-                if ((a >= 1.0d) && (b >= 1.0d) && (a != 1.0d && b != 1.0d))
+                if ((a >= 1) && (b >= 1) && (a != 1 && b != 1))
                 {
-                    return Math.Pow(
-                    (a - 1.0d) /
-                    (a * b - 1.0d)
-                    , 1.0d / a);
+                    double num = a - 1;
+                    double den = a * b - 1;
+                    return Math.Pow(num / den, 1 / a);
                 }
-                return double.NaN;
+
+                return Double.NaN;
             }
         }
 
 
+        /// <summary>
+        ///   Not supported.
+        /// </value>
+        /// 
         public override double Entropy
         {
             get { return double.NaN; }
         }
 
-        public override AForge.DoubleRange Support
+        /// <summary>
+        ///   Gets the support interval for this distribution.
+        /// </summary>
+        /// 
+        /// <value>
+        ///   A <see cref="AForge.DoubleRange" /> containing
+        ///   the support interval for this distribution.
+        /// </value>
+        /// 
+        public override DoubleRange Support
         {
-            get 
-            {
-                return new AForge.DoubleRange( 0.0d, 1.0d );
-            }
+            get { return new DoubleRange(0, 1); }
         }
 
-        public override double DistributionFunction( double x )
+        /// <summary>
+        ///   Gets the cumulative distribution function (cdf) for
+        ///   this distribution evaluated at point <c>x</c>.
+        /// </summary>
+        /// 
+        /// <param name="x">A single point in the distribution range.</param>
+        /// 
+        /// <remarks>
+        ///   The Cumulative Distribution Function (CDF) describes the cumulative
+        ///   probability that a given value or any value smaller than it will occur.
+        /// </remarks>
+        /// 
+        public override double DistributionFunction(double x)
         {
-            return (1.0d - Math.Pow( 1.0d - Math.Pow( x, a ), b ) );
+            if (x > 1) return 1;
+            if (x < 0) return 0;
+
+            double xa = Math.Pow(x, a);
+            return 1 - Math.Pow(1 - xa, b);
         }
 
+        /// <summary>
+        ///   Gets the probability density function (pdf) for
+        ///   this distribution evaluated at point <c>x</c>.
+        /// </summary>
+        /// 
+        /// <param name="x">A single point in the distribution range.</param>
+        /// 
+        /// <returns>
+        ///   The probability of <c>x</c> occurring
+        ///   in the current distribution.
+        /// </returns>
+        /// 
+        /// <remarks>
+        ///   The Probability Density Function (PDF) describes the
+        ///   probability that a given value <c>x</c> will occur.
+        /// </remarks>
+        /// 
         public override double ProbabilityDensityFunction(double x)
         {
-            return a * b * Math.Pow(x, a - 1.0d) * Math.Pow(1.0d - Math.Pow(x, a), b - 1.0d);
+            if (x > 1) return 0;
+            if (x < 0) return 0;
+
+            return a * b * Math.Pow(x, a - 1) * Math.Pow(1 - Math.Pow(x, a), b - 1);
         }
 
+        /// <summary>
+        ///   Creates a new object that is a copy of the current instance.
+        /// </summary>
+        /// 
+        /// <returns>
+        ///   A new object that is a copy of this instance.
+        /// </returns>
+        /// 
         public override object Clone()
         {
             return new KumaraswamyDistribution(this.a, this.b);
@@ -188,8 +297,7 @@ namespace Accord.Statistics.Distributions.Univariate.Continuous
         /// 
         public string ToString(IFormatProvider formatProvider)
         {
-            return String.Format(formatProvider, "Kumaraswamy(x; a = {0}, b = {1})",
-                a, b);
+            return String.Format(formatProvider, "Kumaraswamy(x; a = {0}, b = {1})", a, b);
         }
 
         /// <summary>
--- Unit Tests/Accord.Tests.Statistics/Distributions/Univariate/Continuous/KumaraswamyDistributionTest.cs
+++ Unit Tests/Accord.Tests.Statistics/Distributions/Univariate/Continuous/KumaraswamyDistributionTest.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Globalization;
-using Accord.Statistics.Distributions.Univariate.Continuous;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿// Accord Unit Tests
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © César Souza, 2009-2014
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
 
 namespace Accord.Tests.Statistics.Distributions.Univariate.Continuous
 {
+    using System;
+    using System.Globalization;
+    using Accord.Statistics.Distributions.Univariate.Continuous;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
     [TestClass]
     public class KumaraswamyDistributionTest
     {
@@ -44,7 +66,55 @@ namespace Accord.Tests.Statistics.Distributions.Univariate.Continuous
             Assert.AreEqual(pdf, 0.46195081771596241);
             Assert.AreEqual(cdf, 1.8501524192880519);
             
-            /// values verified in R package = ActuDistns
+            // values verified in R package = ActuDistns
+        }
+
+        [TestMethod()]
+        public void Constructor_ExtensiveTestForDocumentation()
+        {
+            // Create a new Kumaraswamy distribution with shape (4,2)
+            var kumaraswamy = new KumaraswamyDistribution(a: 4, b: 2);
+
+            double mean = kumaraswamy.Mean;     // 0.71111111111111114
+            double median = kumaraswamy.Median; // 0.73566031573423674
+            double mode = kumaraswamy.Mode;     // 0.80910671157022118
+            double var = kumaraswamy.Variance;  // 0.027654320987654302
+
+            double cdf = kumaraswamy.DistributionFunction(x: 0.4);           // 0.050544639999999919
+            double pdf = kumaraswamy.ProbabilityDensityFunction(x: 0.4);     // 0.49889280000000014
+            double lpdf = kumaraswamy.LogProbabilityDensityFunction(x: 0.4); // -0.69536403596913343
+
+            double ccdf = kumaraswamy.ComplementaryDistributionFunction(x: 0.4); // 0.94945536000000008
+            double icdf = kumaraswamy.InverseDistributionFunction(p: cdf);       // 0.40000011480618253
+
+            double hf = kumaraswamy.HazardFunction(x: 0.4);            // 0.52545155993431869
+            double chf = kumaraswamy.CumulativeHazardFunction(x: 0.4); // 0.051866764053008864
+
+            string str = kumaraswamy.ToString(CultureInfo.InvariantCulture); // Kumaraswamy(x; a = 4, b = 2)
+
+            Assert.AreEqual(0.71111111111111114, mean);
+            Assert.AreEqual(0.73566031573423674, median);
+            Assert.AreEqual(0.80910671157022118, mode);
+            Assert.AreEqual(0.027654320987654302, var);
+            Assert.AreEqual(0.051866764053008864, chf);
+            Assert.AreEqual(0.050544639999999919, cdf);
+            Assert.AreEqual(0.49889280000000014, pdf);
+            Assert.AreEqual(-0.69536403596913343, lpdf);
+            Assert.AreEqual(0.52545155993431869, hf);
+            Assert.AreEqual(0.94945536000000008, ccdf);
+            Assert.AreEqual(0.40000011480618253, icdf);
+            Assert.AreEqual("Kumaraswamy(x; a = 4, b = 2)", str);
+
+            var range1 = kumaraswamy.GetRange(0.95);
+            var range2 = kumaraswamy.GetRange(0.99);
+            var range3 = kumaraswamy.GetRange(0.01);
+
+            Assert.AreEqual(0.39890408640473385, range1.Min);
+            Assert.AreEqual(0.93868623055832956, range1.Max);
+            Assert.AreEqual(0.26608174016394209, range2.Min);
+            Assert.AreEqual(0.97400390609144138, range2.Max);
+            Assert.AreEqual(0.26608174016394187, range3.Min);
+            Assert.AreEqual(0.97400390609144138, range3.Max);
         }
     }
 }
