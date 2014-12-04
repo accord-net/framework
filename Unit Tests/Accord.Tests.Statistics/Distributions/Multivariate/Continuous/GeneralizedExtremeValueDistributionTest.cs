9ca445ea3f10a6932081fa65fc2a7c3264d291a4
Ashley Messer <glyphard@gmail.com>
2014-12-03 22:19:07 -0800

    implementation and test for u-quadratic distribution for issue #21

---
+++ Unit Tests/Accord.Tests.Statistics/Distributions/Multivariate/Continuous/GeneralizedExtremeValueDistributionTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Accord.Tests.Statistics.Distributions.Multivariate.Continuous
+{
+    [TestClass]
+    public class GeneralizedExtremeValueDistributionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
