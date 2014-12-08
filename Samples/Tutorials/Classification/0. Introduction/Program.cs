7076f1020f0851acf06b761593d8ddbea8c96b8d
Cesar Souza <cesarsouza@gmail.com>
2014-12-09 00:25:17 +0100

    Updating sample tutorial applications.

--- Samples/Tutorials/Classification/0. Introduction/Program.cs
+++ Samples/Tutorials/Classification/0. Introduction/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using Accord.Controls;
 using Accord.IO;
 using Accord.MachineLearning.Bayes;
@@ -7,13 +8,12 @@ using Accord.MachineLearning.DecisionTrees.Learning;
 using Accord.MachineLearning.VectorMachines;
 using Accord.MachineLearning.VectorMachines.Learning;
 using Accord.Math;
+using Accord.Neuro.Learning;
 using Accord.Statistics.Distributions.Univariate;
 using Accord.Statistics.Kernels;
 using Accord.Statistics.Models.Regression;
 using Accord.Statistics.Models.Regression.Fitting;
-using System;
 using AForge.Neuro;
-using Accord.Neuro.Learning;
 
 namespace ClassificationSample
 {
@@ -43,6 +43,126 @@ namespace ClassificationSample
             logistic(inputs, outputs);
 
             network(inputs, outputs);
+        }
+
+        private static void naiveBayes(double[][] inputs, int[] outputs)
+        {
+            // In our problem, we have 2 classes (samples can be either
+            // positive or negative), and 2 inputs (x and y coordinates).
+
+            var nb = new NaiveBayes<NormalDistribution>(classes: 2,
+                inputs: 2, initial: new NormalDistribution());
+
+            // The Naive Bayes expects the class labels to 
+            // range from 0 to k, so we convert -1 to be 0:
+            //
+            outputs = outputs.Apply(x => x < 0 ? 0 : x);
+
+            // Estimate the Naive Bayes
+            double error = nb.Estimate(inputs, outputs);
+
+            // Classify the samples using the model
+            int[] answers = inputs.Apply(nb.Compute);
+
+            // Plot the results
+            ScatterplotBox.Show("Expected results", inputs, outputs);
+            ScatterplotBox.Show("Naive Bayes results", inputs, answers)
+                .Hold();
+        }
+
+        private static void decisionTree(double[][] inputs, int[] outputs)
+        {
+            // In our problem, we have 2 classes (samples can be either
+            // positive or negative), and 2 continuous-valued inputs.
+            DecisionTree tree = new DecisionTree(inputs: new[] 
+            {
+                DecisionVariable.Continuous("X"),
+                DecisionVariable.Continuous("Y")
+            }, classes: 2);
+
+            C45Learning teacher = new C45Learning(tree);
+
+            // The C4.5 algorithm expects the class labels to
+            // range from 0 to k, so we convert -1 to be zero:
+            //
+            outputs = outputs.Apply(x => x < 0 ? 0 : x);
+
+            double error = teacher.Run(inputs, outputs);
+
+            // Classify the samples using the model
+            int[] answers = inputs.Apply(tree.Compute);
+
+            // Plot the results
+            ScatterplotBox.Show("Expected results", inputs, outputs);
+            ScatterplotBox.Show("Decision Tree results", inputs, answers)
+                .Hold();
+        }
+
+        private static void linearSvm(double[][] inputs, int[] outputs)
+        {
+            // Create a linear binary machine with 2 inputs
+            var svm = new SupportVectorMachine(inputs: 2);
+
+            // Create a L2-regularized L2-loss optimization algorithm for
+            // the dual form of the learning problem. This is *exactly* the
+            // same method used by LIBLINEAR when specifying -s 1 in the 
+            // command line (i.e. L2R_L2LOSS_SVC_DUAL).
+            //
+            var teacher = new LinearCoordinateDescent(svm, inputs, outputs);
+
+            // Teach the vector machine
+            double error = teacher.Run();
+
+            // Classify the samples using the model
+            int[] answers = inputs.Apply(svm.Compute).Apply(System.Math.Sign);
+
+            // Plot the results
+            ScatterplotBox.Show("Expected results", inputs, outputs);
+            ScatterplotBox.Show("LinearSVM results", inputs, answers);
+
+            // Grab the index of multipliers higher than 0
+            int[] idx = teacher.Lagrange.Find(x => x > 0);
+
+            // Select the input vectors for those
+            double[][] sv = inputs.Submatrix(idx);
+
+            // Plot the support vectors selected by the machine
+            ScatterplotBox.Show("Support vectors", sv).Hold();
+        }
+
+        private static void kernelSvm(double[][] inputs, int[] outputs)
+        {
+            // Estimate the kernel from the data
+            var gaussian = Gaussian.Estimate(inputs);
+
+            // Create a Gaussian binary support machine with 2 inputs
+            var svm = new KernelSupportVectorMachine(gaussian, inputs: 2);
+
+            // Create a new Sequential Minimal Optimization (SMO) learning 
+            // algorithm and estimate the complexity parameter C from data
+            var teacher = new SequentialMinimalOptimization(svm, inputs, outputs)
+            {
+                UseComplexityHeuristic = true
+            };
+
+            // Teach the vector machine
+            double error = teacher.Run();
+
+            // Classify the samples using the model
+            int[] answers = inputs.Apply(svm.Compute).Apply(System.Math.Sign);
+
+            // Plot the results
+            ScatterplotBox.Show("Expected results", inputs, outputs);
+            ScatterplotBox.Show("GaussianSVM results", inputs, answers);
+
+            // Grab the index of multipliers higher than 0
+            int[] idx = teacher.Lagrange.Find(x => x > 0);
+
+            // Select the input vectors for those
+            double[][] sv = inputs.Submatrix(idx);
+
+            // Plot the support vectors selected by the machine
+            ScatterplotBox.Show("Support vectors", sv).Hold();
         }
 
         private static void network(double[][] inputs, int[] outputs)
@@ -130,124 +250,5 @@ namespace ClassificationSample
                 .Hold();
         }
 
-        private static void linearSvm(double[][] inputs, int[] outputs)
-        {
-            // Create a linear binary machine with 2 inputs
-            var svm = new SupportVectorMachine(inputs: 2);
-
-            // Create a L2-regularized L2-loss optimization algorithm for
-            // the dual form of the learning problem. This is *exactly* the
-            // same method used by LIBLINEAR when specifying -s 1 in the 
-            // command line (i.e. L2R_L2LOSS_SVC_DUAL).
-            //
-            var teacher = new LinearCoordinateDescent(svm, inputs, outputs);
-
-            // Teach the vector machine
-            double error = teacher.Run();
-
-            // Classify the samples using the model
-            int[] answers = inputs.Apply(svm.Compute).Apply(System.Math.Sign);
-
-            // Plot the results
-            ScatterplotBox.Show("Expected results", inputs, outputs);
-            ScatterplotBox.Show("LinearSVM results", inputs, answers);
-
-            // Grab the index of multipliers higher than 0
-            int[] idx = teacher.Lagrange.Find(x => x > 0);
-
-            // Select the input vectors for those
-            double[][] sv = inputs.Submatrix(idx);
-
-            // Plot the support vectors selected by the machine
-            ScatterplotBox.Show("Support vectors", sv).Hold();
-        }
-
-        private static void kernelSvm(double[][] inputs, int[] outputs)
-        {
-            // Estimate the kernel from the data
-            var gaussian = Gaussian.Estimate(inputs);
-
-            // Create a Gaussian binary support machine with 2 inputs
-            var svm = new KernelSupportVectorMachine(gaussian, inputs: 2);
-
-            // Create a new Sequential Minimal Optimization (SMO) learning 
-            // algorithm and estimate the complexity parameter C from data
-            var teacher = new SequentialMinimalOptimization(svm, inputs, outputs)
-            {
-                UseComplexityHeuristic = true
-            };
-
-            // Teach the vector machine
-            double error = teacher.Run();
-
-            // Classify the samples using the model
-            int[] answers = inputs.Apply(svm.Compute).Apply(System.Math.Sign);
-
-            // Plot the results
-            ScatterplotBox.Show("Expected results", inputs, outputs);
-            ScatterplotBox.Show("GaussianSVM results", inputs, answers);
-
-            // Grab the index of multipliers higher than 0
-            int[] idx = teacher.Lagrange.Find(x => x > 0);
-
-            // Select the input vectors for those
-            double[][] sv = inputs.Submatrix(idx);
-
-            // Plot the support vectors selected by the machine
-            ScatterplotBox.Show("Support vectors", sv).Hold();
-        }
-
-        private static void decisionTree(double[][] inputs, int[] outputs)
-        {
-            // In our problem, we have 2 classes (samples can be either
-            // positive or negative), and 2 continuous-valued inputs.
-            DecisionTree tree = new DecisionTree(attributes: new[] 
-            {
-                DecisionVariable.Continuous("X"),
-                DecisionVariable.Continuous("Y")
-            }, outputClasses: 2);
-
-            C45Learning teacher = new C45Learning(tree);
-
-            // The C4.5 algorithm expects the class labels to
-            // range from 0 to k, so we convert -1 to be zero:
-            //
-            outputs = outputs.Apply(x => x < 0 ? 0 : x);
-
-            double error = teacher.Run(inputs, outputs);
-
-            // Classify the samples using the model
-            int[] answers = inputs.Apply(tree.Compute);
-
-            // Plot the results
-            ScatterplotBox.Show("Expected results", inputs, outputs);
-            ScatterplotBox.Show("Decision Tree results", inputs, answers)
-                .Hold();
-        }
-
-        private static void naiveBayes(double[][] inputs, int[] outputs)
-        {
-            // In our problem, we have 2 classes (samples can be either
-            // positive or negative), and 2 inputs (x and y coordinates).
-
-            var nb = new NaiveBayes<NormalDistribution>(classes: 2,
-                inputs: 2, prior: new NormalDistribution());
-
-            // The Naive Bayes expects the class labels to 
-            // range from 0 to k, so we convert -1 to be 0:
-            //
-            outputs = outputs.Apply(x => x < 0 ? 0 : x);
-
-            // Estimate the Naive Bayes
-            double error = nb.Estimate(inputs, outputs);
-
-            // Classify the samples using the model
-            int[] answers = inputs.Apply(nb.Compute);
-
-            // Plot the results
-            ScatterplotBox.Show("Expected results", inputs, outputs);
-            ScatterplotBox.Show("Naive Bayes results", inputs, answers)
-                .Hold();
-        }
     }
 }
