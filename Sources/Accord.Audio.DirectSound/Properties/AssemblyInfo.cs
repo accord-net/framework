32cb2e0fb89cd28f5114a838c4cbde025160a050
Cesar Souza <cesarsouza@gmail.com>
2015-05-02 12:35:26 +0200

    Unifying versioning system.

--- Sources/Accord.Audio.DirectSound/Properties/AssemblyInfo.cs
+++ Sources/Accord.Audio.DirectSound/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Accord.DirectSound")]
+[assembly: AssemblyTitle("Accord.Audio.DirectSound")]
 [assembly: AssemblyDescription("Accord.NET - Direct Sound Library")]
 [assembly: AssemblyConfiguration("")]
 
