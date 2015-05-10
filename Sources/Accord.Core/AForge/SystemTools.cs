946b1367266c03d84d216fa90ce91c4c25864ce9
Cesar Souza <cesarsouza@gmail.com>
2015-05-10 14:15:59 +0200

    Removing Mono-specific compilation options and switching to dll remapping instead.

--- Sources/Accord.Core/AForge/SystemTools.cs
+++ Sources/Accord.Core/AForge/SystemTools.cs
@@ -36,12 +36,9 @@ namespace AForge
         /// not provide any way to copy unmanaged blocks, but provides only methods to
         /// copy from unmanaged memory to managed memory and vise versa.</para></remarks>
         ///
-        public static IntPtr CopyUnmanagedMemory( IntPtr dst, IntPtr src, int count )
+        public unsafe static IntPtr CopyUnmanagedMemory(IntPtr dst, IntPtr src, int count)
         {
-            unsafe
-            {
-                CopyUnmanagedMemory( (byte*) dst.ToPointer( ), (byte*) src.ToPointer( ), count );
-            }
+            CopyUnmanagedMemory((byte*)dst.ToPointer(), (byte*)src.ToPointer(), count);
             return dst;
         }
 
@@ -59,31 +56,9 @@ namespace AForge
         /// not provide any way to copy unmanaged blocks, but provides only methods to
         /// copy from unmanaged memory to managed memory and vise versa.</para></remarks>
         /// 
-        public static unsafe byte* CopyUnmanagedMemory( byte* dst, byte* src, int count )
+        public static unsafe byte* CopyUnmanagedMemory(byte* dst, byte* src, int count)
         {
-#if !MONO
-            return memcpy( dst, src, count );
-#else
-            int countUint = count >> 2;
-            int countByte = count & 3;
-
-            uint* dstUint = (uint*) dst;
-            uint* srcUint = (uint*) src;
-
-            while ( countUint-- != 0 )
-            {
-                *dstUint++ = *srcUint++;
-            }
-
-            byte* dstByte = (byte*) dstUint;
-            byte* srcByte = (byte*) srcUint;
-
-            while ( countByte-- != 0 )
-            {
-                *dstByte++ = *srcByte++;
-            }
-            return dst;
-#endif
+            return memcpy(dst, src, count);
         }
 
         /// <summary>
@@ -96,12 +71,9 @@ namespace AForge
         /// 
         /// <returns>Return's value of <paramref name="dst"/> - pointer to destination.</returns>
         /// 
-        public static IntPtr SetUnmanagedMemory( IntPtr dst, int filler, int count )
+        public unsafe static IntPtr SetUnmanagedMemory(IntPtr dst, int filler, int count)
         {
-            unsafe
-            {
-                SetUnmanagedMemory( (byte*) dst.ToPointer( ), filler, count );
-            }
+            SetUnmanagedMemory((byte*)dst.ToPointer(), filler, count);
             return dst;
         }
 
@@ -115,50 +87,19 @@ namespace AForge
         /// 
         /// <returns>Return's value of <paramref name="dst"/> - pointer to destination.</returns>
         /// 
-        public static unsafe byte* SetUnmanagedMemory( byte* dst, int filler, int count )
+        public static unsafe byte* SetUnmanagedMemory(byte* dst, int filler, int count)
         {
-#if !MONO
-            return memset( dst, filler, count );
-#else
-            int countUint = count >> 2;
-            int countByte = count & 3;
-
-            byte fillerByte = (byte) filler;
-            uint fiilerUint = (uint) filler | ( (uint) filler << 8 ) |
-                                              ( (uint) filler << 16 );// |
-                                              //( (uint) filler << 24 );
-
-            uint* dstUint = (uint*) dst;
-
-            while ( countUint-- != 0 )
-            {
-                *dstUint++ = fiilerUint;
-            }
-
-            byte* dstByte = (byte*) dstUint;
-
-            while ( countByte-- != 0 )
-            {
-                *dstByte++ = fillerByte;
-            }
-            return dst;
-#endif
+            return memset(dst, filler, count);
         }
 
 
-#if !MONO
         // Win32 memory copy function
-        [DllImport( "ntdll.dll", CallingConvention = CallingConvention.Cdecl )]
-        private static unsafe extern byte* memcpy(
-            byte* dst,
-            byte* src,
-            int count );
+        [DllImport("ntdll.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static unsafe extern byte* memcpy(byte* dst, byte* src, int count);
+
         // Win32 memory set function
-        [DllImport( "ntdll.dll", CallingConvention = CallingConvention.Cdecl )]
-        private static unsafe extern byte* memset(
-            byte* dst,
-            int filler,
-            int count );
-#endif
+        [DllImport("ntdll.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static unsafe extern byte* memset(byte* dst, int filler, int count);
+
     }
 }
