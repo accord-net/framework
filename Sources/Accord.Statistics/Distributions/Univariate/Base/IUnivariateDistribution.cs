25376342cd6355960a83d42677f0321122d4ef5f
Cesar Souza <cesarsouza@gmail.com>
2015-05-21 21:48:03 +0200

    Fixing Mono build.

--- Sources/Accord.Statistics/Distributions/Univariate/Base/IUnivariateDistribution.cs
+++ Sources/Accord.Statistics/Distributions/Univariate/Base/IUnivariateDistribution.cs
@@ -360,7 +360,7 @@ namespace Accord.Statistics.Distributions
         /// <param name="p">A probability value between 0 and 1.</param>
         /// 
         /// <returns>A sample which could original the given probability 
-        ///   value when applied in the <see cref="IDistribution{T}.DistributionFunction"/>.</returns>
+        ///   value when applied in the <see cref="IUnivariateDistribution.DistributionFunction(double)"/>.</returns>
         /// 
         TObservation InverseDistributionFunction(double p);
 
