c7fa2b8ce290360bcf0ca3c93bd774642ada38e1
Cesar Souza <cesarsouza@gmail.com>
2015-06-19 00:02:45 +0200

    Working on documentation.

---
+++ Sources/Accord.Docs/Namespaces/Accord.Imaging.cs
@@ -0,0 +1,62 @@
+﻿// Accord Statistics Library
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © César Souza, 2009-2015
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+namespace Accord.Imaging
+{
+    using System.Runtime.CompilerServices;
+    using Accord.Imaging.Converters;
+    using Accord.Imaging.Filters;
+    using Accord.Imaging.Moments;
+
+    /// <summary>
+    ///  Contains Interest point detectors (i.e. Harris, SURF and FAST), image matching and image stitching methods.
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// <para>
+    ///   This namespace contains several image processing methods, from corner detectors such as
+    ///   <see cref="HarrisCornersDetector">Harris</see> and <see cref="FastCornersDetector">FAST</see>,
+    ///   as well as feature detectors like <a href="SpeedUpRobustFeatureDetector">SURF</a>, 
+    ///   <see cref="LocalBinaryPattern">LBP</see>, and texture feature extractors such as 
+    ///   <see cref="Haralick"/>, to essential image classification tools such as <see cref="BagOfVisualWords"/>,
+    ///   <see cref="KNearestNeighborMatching{T}"/> and computer vision tools like <see cref="RansacFundamentalEstimator"/>
+    ///   and <see cref="RansacHomographyEstimator"/>. </para>
+    ///   
+    /// <para>
+    ///   It also includes several image filters like <see cref="GaborFilter">Gabor filters</see>,
+    ///   <see cref="WolfJolionThreshold">Wolf-Jolion</see> and <see cref="NiblackThreshold">Niblack</see>
+    ///   thresholding, <see cref="Kuwahara"/> and <see cref="GrayWorld">Gray World</see>.</para>
+    ///   
+    /// <para>
+    ///   The namespace class diagram is shown below. </para>
+    ///   <img src="..\diagrams\classes\Accord.Imaging.png" />
+    /// </remarks>
+    ///   
+    /// <seealso cref="Accord.Imaging.Converters"/>
+    /// <seealso cref="Accord.Imaging.Filters"/>
+    /// <seealso cref="Accord.Imaging.Moments"/>
+    ///   
+    [CompilerGenerated]
+    class NamespaceDoc
+    {
+    }
+}
