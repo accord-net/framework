5110e5257ea46d1a8fd4b3e3d7dd2f266ac00767
Cesar Souza <cesarsouza@gmail.com>
2015-06-19 18:03:54 +0200

    Updating unit tests script for Mono.

--- Tests.sh
+++ Tests.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 ###########
 
-TESTS="Unit\ Tests/bin/Release/net45/"
-NUNIT="Externals/NUnit/nunit-console-x86.exe" 
+TESTS="Unit Tests/bin/Release/net40/"
+NUNIT="Externals/NUnit/nunit-console-x86.exe"
+LIST=()
 
 # Run unit tests in the Mono solution
-LIST="$LIST '%TESTS%Accord.Tests.Audio.dll"
-LIST="$LIST '%TESTS%Accord.Tests.Controls.dll"
-LIST="$LIST '%TESTS%Accord.Tests.Core.dll"
-LIST="$LIST '%TESTS%Accord.Tests.Imaging.dll" 
-LIST="$LIST '%TESTS%Accord.Tests.IO.dll"
-LIST="$LIST '%TESTS%Accord.Tests.MachineLearning.dll"
-LIST="$LIST '%TESTS%Accord.Tests.Math.dll"
-LIST="$LIST '%TESTS%Accord.Tests.Neuro.dll" 
-LIST="$LIST '%TESTS%Accord.Tests.Vision.dll" 
-LIST="$LIST '%TESTS%Accord.Tests.Statistics.dll" 
+LIST+=("${TESTS}Accord.Tests.Audio.dll")
+LIST+=("${TESTS}Accord.Tests.Controls.dll")
+LIST+=("${TESTS}Accord.Tests.Core.dll")
+#LIST="$LIST ${TESTS}Accord.Tests.Imaging.dll
+#LIST="$LIST ${TESTS}Accord.Tests.IO.dll
+#LIST="$LIST ${TESTS}Accord.Tests.MachineLearning.dll
+#LIST="$LIST ${TESTS}Accord.Tests.Math.dll
+#LIST="$LIST ${TESTS}Accord.Tests.Neuro.dll
+#LIST="$LIST ${TESTS}Accord.Tests.Vision.dll
+#LIST="$LIST ${TESTS}Accord.Tests.Statistics.dll
 
-mono --runtime=v4.0 $NUNIT -noxml -nodots -labels -stoponerror /process=multiple $LIST /framework:net-4.0
+
+mono --runtime=v4.0 ${NUNIT} -noxml -nodots -labels -process=multiple "${LIST[@]/#/}"
