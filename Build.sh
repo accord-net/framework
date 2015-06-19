5107e785f90862a36d1d564927941a83b70d8e27
Cesar Souza <cesarsouza@gmail.com>
2015-06-19 20:58:05 +0200

    Removing invalid entries from sample applications' resource files.

--- Build.sh
+++ Build.sh
@@ -9,16 +9,16 @@ echo "This Linux bash script will use Mono's xbuild tool to    "
 echo "compile the Debug and Release versions of the framework. "
 echo "                                                         " 
 
-export XBUILD_COLORS=errors=brightred,warnings=blue
 
-if [[ $# -eq 0 || $1 -eq "framework" ]]; then
+
+if [ $# -eq 0 ] || [ "$1" == "framework" ]; then
 	echo ""
 	echo "  - Building NET40 configuration..."
 	echo ""
 	xbuild /p:Configuration=NET40 Sources/Accord.NET.Mono.sln 
 fi
 
-if [[ $# -eq 0 || $1 -eq "samples" ]]; then
+if [ $# -eq 0 ] || [ "$1" == "samples" ]; then
 	echo ""
 	echo "  - Building samples..."
 	echo ""
