4f211aa043c34a6333f4da045de788800cd17fb5
Cesar Souza <cesarsouza@gmail.com>
2015-06-19 20:35:19 +0200

    Updating Travis build scripts.

--- Build.sh
+++ Build.sh
@@ -9,10 +9,18 @@ echo "This Linux bash script will use Mono's xbuild tool to    "
 echo "compile the Debug and Release versions of the framework. "
 echo "                                                         " 
 
+export XBUILD_COLORS=errors=brightred,warnings=blue
 
-echo "  - Building NET40 configuration..."
-xbuild /p:Configuration=NET40 Sources/Accord.NET.Mono.sln 
+if [[ $# -eq 0 || $1 -eq "framework" ]]; then
+	echo ""
+	echo "  - Building NET40 configuration..."
+	echo ""
+	xbuild /p:Configuration=NET40 Sources/Accord.NET.Mono.sln 
+fi
 
-
-echo "  - Building samples..."
-xbuild /p:Configuration=Release /p:Platform=x86 Samples/Samples.sln
+if [[ $# -eq 0 || $1 -eq "samples" ]]; then
+	echo ""
+	echo "  - Building samples..."
+	echo ""
+	xbuild /p:Configuration=Release /p:Platform=x86 Samples/Samples.sln
+fi
