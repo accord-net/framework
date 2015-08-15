94cec18a153e64a5b6c5b5cbaf7d6d1f3bf45916
Marcos Diego Catalano <diego.catalano@live.com>
2015-08-15 12:03:30 -0300

    Thin Plate Spline
    - New: Thin Plate Spline kernel.

---
+++ Sources/Accord.Statistics/Kernels/ThinPlateSpline.cs
@@ -0,0 +1,100 @@
+﻿// Accord Statistics Library
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © Diego Catalano, 2015
+// diego.catalano at live.com
+//
+// Copyright 2015 Haifeng Li
+// haifeng.hli at gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Accord.Statistics.Kernels
+{
+    using System;
+
+    /// <summary>
+    ///   Thin Plate Spline Kernel.
+    /// </summary>
+    /// 
+    /// <remarks>
+    ///   Thin plate splines (TPS) are a spline-based technique for data interpolation and smoothing.
+    /// </remarks>
+    /// 
+    [Serializable]
+    public sealed class ThinPlateSpline : KernelBase, IKernel, ICloneable
+    {
+        private double sigma;
+
+        /// <summary>
+        ///   Gets or sets the sigma constant for this kernel.
+        /// </summary>
+        /// 
+        public double Sigma
+        {
+            get { return sigma; }
+            set { sigma = Math.Max(value,1); }
+        }
+
+        /// <summary>
+        ///   Constructs a new ThinPlateSpline Kernel.
+        /// </summary>
+        public ThinPlateSpline()
+        {
+            Sigma = 1;
+        }
+
+        /// <summary>
+        ///   Constructs a new ThinPlateSpline Kernel.
+        /// </summary>
+        /// <param name="sigma">The value for sigma.</param>
+        public ThinPlateSpline(double sigma)
+        {
+            Sigma = sigma;
+        }
+
+        /// <summary>
+        ///   Thin Plate Spline Kernel.
+        /// </summary>
+        /// 
+        /// <param name="x">Vector <c>x</c> in input space.</param>
+        /// <param name="y">Vector <c>y</c> in input space.</param>
+        /// <returns>Dot product in feature (kernel) space.</returns>
+        /// 
+        public override double Function(double[] x, double[] y)
+        {
+            double r = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                double dxy = x[i] - y[i];
+                r += dxy * dxy;
+            }
+
+            return r / (sigma * sigma) * Math.Log(Math.Sqrt(r) / sigma);
+        }
+
+        /// <summary>
+        ///   Creates a new object that is a copy of the current instance.
+        /// </summary>
+        /// 
+        /// <returns>
+        ///   A new object that is a copy of this instance.
+        /// </returns>
+        /// 
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
