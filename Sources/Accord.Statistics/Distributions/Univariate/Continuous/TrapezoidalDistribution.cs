70cee798586e4497032156d3a293ba32b70c44e5
Cesar Souza <cesarsouza@gmail.com>
2015-08-15 02:29:13 +0200

    Correcting the namespace location for some continuous statistical distributions.

--- Sources/Accord.Statistics/Distributions/Univariate/Continuous/TrapezoidalDistribution.cs
+++ Sources/Accord.Statistics/Distributions/Univariate/Continuous/TrapezoidalDistribution.cs
@@ -19,7 +19,7 @@
 //    License along with this library; if not, write to the Free Software
 //    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
-namespace Accord.Statistics.Distributions.Univariate.Continuous
+namespace Accord.Statistics.Distributions.Univariate
 {
     using System;
     using AForge;
--- Sources/Accord.Statistics/Distributions/Univariate/Continuous/UQuadraticDistribution.cs
+++ Sources/Accord.Statistics/Distributions/Univariate/Continuous/UQuadraticDistribution.cs
@@ -20,7 +20,7 @@
 //    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
-namespace Accord.Statistics.Distributions.Univariate.Continuous
+namespace Accord.Statistics.Distributions.Univariate
 {
     using System;
     using Accord.Math;
