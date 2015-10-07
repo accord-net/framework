f333485e0823963f42de4e95b28391fd5e0264b9
Alex Risman <alex.risman@mcmaster.com>
2015-10-07 14:23:00 -0500

    random forest predict and fit

---
+++ Sources/Accord.MachineLearning/DecisionTrees/ForestTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Accord.MachineLearning.DecisionTrees;
+using Accord.Statistics.Filters;
+using Accord.MachineLearning.DecisionTrees.Learning;
+using Accord.Math;
+
+
+namespace Accord.MachineLearning.DecisionTrees
+{
+    class ForestTree
+    {
+        private static Codification mCodebook = null;
+        private static string[] mInputCols = null;
+        private static string mOutputCol = null;
+        private DecisionTree mTree = null;
+
+        public ForestTree(string[] inputCols, string outputCol)
+        {
+            mInputCols = inputCols;
+            mOutputCol = outputCol;
+        }
+
+        public void Fit(DataTable data)
+        {
+            mCodebook = new Codification(data);
+            DataTable symbols = mCodebook.Apply(data);
+            double[][] inputs = symbols.ToArray(mInputCols);
+            int[] outputs = symbols.ToArray<int>(mOutputCol);
+            DecisionVariable[] attributes = DecisionVariable.FromCodebook(mCodebook, mInputCols);
+            mTree = new DecisionTree(attributes, 2);
+            C45Learning c45 = new C45Learning(mTree);
+            c45.Run(inputs, outputs);
+        }
+
+        public int[] Predict(DataTable data)
+        {
+            DataTable symbols = mCodebook.Apply(data);
+            double[][] inputs = symbols.ToArray(mInputCols);
+            return inputs.Select(x => predict(x)).ToArray();
+        }
+
+        private int predict(double[] data)
+        {
+            return mTree.Compute(data);
+        }
+
+    }
+}
---
+++ Sources/Accord.MachineLearning/DecisionTrees/RandomForest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Accord.Statistics.Filters;
+
+
+namespace Accord.MachineLearning.DecisionTrees
+{
+    public class RandomForest
+    {
+        private List<ForestTree> mTrees = new List<ForestTree>();
+        private List<double[][]> mSubsets = new List<double[][]>();
+        private static int mNRows;
+        private static int mNCols;
+        private static int mNTrees;
+        private static double mSizeOfRandomSample;
+        private static double mNColsPerRandomSample;
+        private static string[] mInputColumns;
+        private static string mOutputColumn;
+        private static DataTable mData;
+        private static Codification mCodebook;
+
+        public RandomForest(double maxFeatures = 0, double sizeOfRandomSample = .632, int nTrees = 100)
+        {
+            mNColsPerRandomSample = maxFeatures;
+            mSizeOfRandomSample = sizeOfRandomSample;
+            mNTrees = nTrees;
+        }
+
+        public void Fit(DataTable data, string[] inputColumns, string outputColumn)
+        {
+            mNRows = data.Rows.Count;
+            mNCols = data.Columns.Count - 1;
+            if (mNColsPerRandomSample == 0)
+            {
+                mNColsPerRandomSample = System.Math.Sqrt(mNCols) / mNCols;
+            }
+            mInputColumns = inputColumns;
+            mOutputColumn = outputColumn;
+            mData = data;
+            createForest();
+        }
+
+        public string[] Predict(DataTable data, double threshold)
+        {
+            int[][] treePreds = mTrees.Select(x => x.Predict(data)).ToArray();
+            List<double> predProbs = new List<double>();
+            int i = 0;
+            while(i < data.Rows.Count)
+            {
+                predProbs.Add(treePreds.Select(x => x[i]).Average());
+                i += 1;
+            }
+            return predProbs.Select(x => mCodebook.Translate(mOutputColumn, Convert.ToInt32(x > threshold))).ToArray();
+
+        }
+
+
+        private void createForest()
+        {
+            int i = 0;
+            while (i < mNTrees)
+            {
+                Random rnd = new Random();
+                DataTable dataSubset = mData.AsEnumerable().Where(x => rnd.Next(100) < mSizeOfRandomSample * 100).CopyToDataTable();
+                int classCnt = dataSubset.AsEnumerable().Select(y => y.Field<object>(mOutputColumn)).Distinct().Count();
+                while (classCnt < 2)
+                {
+                    dataSubset = mData.AsEnumerable().Where(x => rnd.Next(100) < mSizeOfRandomSample * 100).CopyToDataTable();
+                    classCnt = dataSubset.AsEnumerable().Select(y => y.Field<object>(mOutputColumn)).Distinct().Count();
+                }
+                string[] inputColSubset = mInputColumns.Where(x => rnd.Next(100) < mNColsPerRandomSample * 100).ToArray();
+                inputColSubset = inputColSubset.Where(x => dataSubset.AsEnumerable().Select(y => y.Field<object>(x)).Distinct().Count() > 1).ToArray();
+                ForestTree tree = new ForestTree(inputColSubset, mOutputColumn);
+                tree.Fit(dataSubset);
+                mTrees.Add(tree);
+                i += 1;
+            }
+
+
+            
+
+
+            
+        }
+
+    }
+}
