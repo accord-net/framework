3841295dac0e67d9e81e8bd013411e916bd75517
Cesar Souza <cesarsouza@gmail.com>
2016-07-01 20:27:32 +0200

    Adding a very basic implementation for the apriori algorithm.

---
+++ Sources/Accord.MachineLearning/Rules/Apriori.cs
@@ -0,0 +1,245 @@
+﻿// Accord Machine Learning Library
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © César Souza, 2009-2016
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+namespace Accord.MachineLearning.Rules
+{
+    using Accord.Math;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    ///   A-priori algorithm for association rule mining.
+    /// </summary>
+    /// 
+    public class Apriori : Apriori<int>
+    {
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="Apriori"/> class.
+        /// </summary>
+        /// <param name="threshold">The minimum number of times a rule should be detected (also known as its support) 
+        ///   before it can be registered as a permanent in the learned classifier.</param>
+        /// <param name="confidence">The minimum confidence in an association rule beore it is 
+        ///   registered.</param>
+        ///   
+        public Apriori(int threshold, double confidence)
+            : base(threshold, confidence)
+        {
+        }
+    }
+
+    /// <summary>
+    ///   A-priori algorithm for association rule mining.
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// <para>
+    ///   References:
+    ///   <list type="bullet">
+    ///     <item><description>
+    ///       Anita Wasilewska, Lecture Notes. Available on 
+    ///       http://www3.cs.stonybrook.edu/~cse634/lecture_notes/07apriori.pdf </description></item>
+    ///    </list>></para>
+    /// </remarks>
+    ///
+    public class Apriori<T> :
+        IUnsupervisedLearning<AssociationRuleMatcher<T>, SortedSet<T>, SortedSet<T>[]>,
+        IUnsupervisedLearning<AssociationRuleMatcher<T>, T[], T[][]>
+    {
+        private int supportMin;
+        private double confidence;
+        private Dictionary<SortedSet<T>, int> frequent;
+
+        /// <summary>
+        ///   Gets the set of most frequent items and the respective 
+        ///   number of times their appear in in the training dataset.
+        /// </summary>
+        /// 
+        public Dictionary<SortedSet<T>, int> Frequent
+        {
+            get { return frequent; }
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Apriori{T}"/> class.
+        /// </summary>
+        /// 
+        /// <param name="threshold">The minimum number of times a rule should be detected (also known as its support) 
+        ///   before it can be registered as a permanent in the learned classifier.</param>
+        /// <param name="confidence">The minimum confidence in an association rule beore it is 
+        ///   registered.</param>
+        ///   
+        public Apriori(int threshold, double confidence)
+        {
+            this.supportMin = threshold;
+            this.confidence = confidence;
+            this.frequent = new Dictionary<SortedSet<T>, int>();
+        }
+
+
+        /// <summary>
+        ///   Learns a model that can map the given inputs to the desired outputs.
+        /// </summary>
+        /// 
+        /// <param name="x">The model inputs.</param>
+        /// 
+        /// <returns>A model that has learned how to produce suitable outputs
+        ///   given the input data <paramref name="x"/>.</returns>
+        /// 
+        public AssociationRuleMatcher<T> Learn(T[][] x)
+        {
+            return Learn(x.Apply(xi => new SortedSet<T>(xi)));
+        }
+
+        /// <summary>
+        ///   Learns a model that can map the given inputs to the desired outputs.
+        /// </summary>
+        /// 
+        /// <param name="x">The model inputs.</param>
+        /// 
+        /// <returns>A model that has learned how to produce suitable outputs
+        ///   given the input data <paramref name="x"/>.</returns>
+        /// 
+        public AssociationRuleMatcher<T> Learn(SortedSet<T>[] x)
+        {
+            frequent.Clear();
+            var L = new HashSet<SortedSet<T>>(new Comparer());
+            var counts = new Dictionary<SortedSet<T>, int>(new Comparer());
+
+            foreach (var t in x)
+                foreach (var s in t)
+                    L.Add(new SortedSet<T>() { s });
+            int items = L.Count;
+
+            for (int k = 1; L.Count != 0; k++)
+            {
+                var C = fold(L, k);
+                counts.Clear();
+                foreach (SortedSet<T> transaction in x)
+                {
+                    foreach (SortedSet<T> candidate in C)
+                    {
+                        if (candidate.IsSubsetOf(transaction))
+                        {
+                            int count;
+                            if (counts.TryGetValue(candidate, out count))
+                                counts[candidate] = count + 1;
+                            else
+                                counts[candidate] = 1;
+                        }
+                    }
+                }
+
+                L.Clear();
+                foreach (var pair in counts)
+                {
+                    if (pair.Value >= supportMin)
+                    {
+                        L.Add(pair.Key);
+                        frequent.Add(pair.Key, pair.Value);
+                    }
+                }
+            }
+
+            var rules = new List<AssociationRule<T>>();
+
+            // Generate association rules from the most frequent itemsets
+            foreach (var itemset in frequent.Keys)
+            {
+                double total = support(itemset, x);
+
+                // generate all non-empty subsets of I
+                foreach (var s in itemset.Subsets())
+                {
+                    double subtotal = support(s, x);
+                    double sc = total / subtotal;
+                    if (sc >= confidence && subtotal >= supportMin)
+                    {
+                        var y = new SortedSet<T>(itemset);
+                        y.ExceptWith(s);
+                        if (y.Count > 0)
+                        {
+                            rules.Add(new AssociationRule<T>()
+                            {
+                                Confidence = sc,
+                                Support = total,
+                                X = s,
+                                Y = y,
+                            });
+                        }
+                    }
+                }
+            }
+
+            return new AssociationRuleMatcher<T>(items, rules.ToArray());
+        }
+
+        private int support(ISet<T> set, IList<SortedSet<T>> dataset)
+        {
+            int count = 0;
+            foreach (var itemset in dataset)
+                if (set.IsSubsetOf(itemset))
+                    count++;
+
+            return count;
+        }
+
+
+        private class Comparer : IEqualityComparer<SortedSet<T>>
+        {
+            public bool Equals(SortedSet<T> x, SortedSet<T> y)
+            {
+                return x.SetEquals(y);
+            }
+
+            public int GetHashCode(SortedSet<T> obj)
+            {
+                // TODO: Change to something more efficient
+                int sum = 0;
+                foreach (var i in obj)
+                    sum ^= i.GetHashCode();
+                return sum;
+            }
+        }
+
+        HashSet<SortedSet<T>> fold(HashSet<SortedSet<T>> L, int k)
+        {
+            var r = new HashSet<SortedSet<T>>(new Comparer());
+
+            foreach (var i in L)
+            {
+                foreach (var j in L)
+                {
+                    var t = new SortedSet<T>(i);
+                    t.UnionWith(j);
+                    if (t.Count == k)
+                        r.Add(t);
+                }
+            }
+
+            return r;
+        }
+    }
+}
---
+++ Sources/Accord.MachineLearning/Rules/AssociationRuleMatcher.cs
@@ -0,0 +1,430 @@
+﻿// Accord Machine Learning Library
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © César Souza, 2009-2016
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+namespace Accord.MachineLearning.Rules
+{
+    using Accord.Math;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    ///   Association ruler matcher.
+    /// </summary>
+    /// 
+    /// <typeparam name="T">The item type.</typeparam>
+    /// 
+    [Serializable]
+    public class AssociationRuleMatcher<T> :
+        IMulticlassRefDistanceClassifier<T[], T[][]>,
+        IMulticlassRefDistanceClassifier<SortedSet<T>, SortedSet<T>[]>
+    {
+        int items;
+        AssociationRule<T>[] rules;
+        double threshold;
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="AssociationRuleMatcher{T}"/> class.
+        /// </summary>
+        /// 
+        /// <param name="items">The number of distinct items in the dataset.</param>
+        /// <param name="rules">The association rules between items of the dataset.</param>
+        /// 
+        public AssociationRuleMatcher(int items, AssociationRule<T>[] rules)
+        {
+            this.items = items;
+            this.rules = rules;
+        }
+
+        /// <summary>
+        ///   Gets the number of items seen by the model during training.
+        /// </summary>
+        /// 
+        public int NumberOfInputs
+        {
+            get { return items; }
+        }
+
+        /// <summary>
+        ///   Gets the number of association rules seen by the model.
+        /// </summary>
+        public int NumberOfOutputs
+        {
+            get { return rules.Length; }
+        }
+
+        /// <summary>
+        ///   Gets or sets the association rules in this model.
+        /// </summary>
+        /// 
+        public AssociationRule<T>[] Rules
+        {
+            get { return rules; }
+            set { rules = value; }
+        }
+
+        /// <summary>
+        ///   Gets or sets the minimum confidence threshold used to 
+        ///   determine whether a rule applies to an input or not.
+        /// </summary>
+        /// 
+        public double Threshold
+        {
+            get { return threshold; }
+            set { threshold = value; }
+        }
+
+
+
+        /// <summary>
+        /// Predicts a class label vector for the given input vector, returning a
+        /// numerical score measuring the strength of association of the input vector
+        /// to each of the possible classes.
+        /// </summary>
+        /// <param name="input">A set of input vectors.</param>
+        /// <param name="decision">The class labels associated with each input
+        /// vector, as predicted by the classifier. If passed as null, the classifier
+        /// will create a new array.</param>
+        public double[] Distances(SortedSet<T> input, ref SortedSet<T>[] decision)
+        {
+            var matchs = new List<SortedSet<T>>();
+            var scores = new List<double>();
+            foreach (var rule in rules)
+            {
+                if (rule.Matches(input))
+                {
+                    if (rule.Confidence > threshold)
+                    {
+                        matchs.Add(rule.Y);
+                        scores.Add(rule.Confidence);
+                    }
+                }
+            }
+
+            decision = matchs.ToArray();
+            return scores.ToArray();
+        }
+
+        /// <summary>
+        /// Predicts a class label vector for the given input vector, returning a
+        /// numerical score measuring the strength of association of the input vector
+        /// to each of the possible classes.
+        /// </summary>
+        /// <param name="input">A set of input vectors.</param>
+        /// <param name="decision">The class labels associated with each input
+        /// vector, as predicted by the classifier. If passed as null, the classifier
+        /// will create a new array.</param>
+        /// <param name="result">An array where the distances will be stored,
+        ///   avoiding unnecessary memory allocations.</param>
+        ///   
+        public double[] Distances(SortedSet<T> input, ref SortedSet<T>[] decision, double[] result)
+        {
+            var r = Distances(input, ref decision);
+            Array.Copy(r, result, r.Length);
+            return result;
+        }
+
+        /// <summary>
+        /// Computes a class-label decision for a given <paramref name="input" />.
+        /// </summary>
+        /// <param name="input">The input vector that should be classified into
+        /// one of the <see cref="ITransform.NumberOfOutputs" /> possible classes.</param>
+        /// <returns>
+        /// A class-label that best described <paramref name="input" /> according
+        /// to this classifier.
+        /// </returns>
+        public SortedSet<T>[] Decide(SortedSet<T> input)
+        {
+            var matchs = new List<SortedSet<T>>();
+            foreach (var rule in rules)
+                if (rule.Matches(input))
+                    if (rule.Confidence > threshold)
+                        matchs.Add(rule.Y);
+
+            return matchs.ToArray();
+        }
+
+        /// <summary>
+        /// Computes a class-label decision for a given <paramref name="input" />.
+        /// </summary>
+        /// <param name="input">The input vector that should be classified into
+        /// one of the <see cref="ITransform.NumberOfOutputs" /> possible classes.</param>
+        /// <returns>
+        /// A class-label that best described <paramref name="input" /> according
+        /// to this classifier.
+        /// </returns>
+        public SortedSet<T>[][] Decide(SortedSet<T>[] input)
+        {
+            var result = new SortedSet<T>[input.Length][];
+            return Decide(input, result);
+        }
+
+        /// <summary>
+        /// Computes a class-label decision for a given <paramref name="input" />.
+        /// </summary>
+        /// <param name="input">The input vector that should be classified into
+        /// one of the <see cref="ITransform.NumberOfOutputs" /> possible classes.</param>
+        /// <param name="result">An array where the distances will be stored,
+        ///   avoiding unnecessary memory allocations.</param>
+        /// <returns>
+        /// A class-label that best described <paramref name="input" /> according
+        /// to this classifier.
+        /// </returns>
+        public SortedSet<T>[][] Decide(SortedSet<T>[] input, SortedSet<T>[][] result)
+        {
+            for (int i = 0; i < input.Length; i++)
+                result[i] = Decide(input[i]);
+            return result;
+        }
+
+        /// <summary>
+        /// Predicts a class label vector for each input vector, returning a
+        /// numerical score measuring the strength of association of the input vector
+        /// to each of the possible classes.
+        /// </summary>
+        /// <param name="input">A set of input vectors.</param>
+        /// <param name="decision">The class labels associated with each input
+        /// vector, as predicted by the classifier. If passed as null, the classifier
+        /// will create a new array.</param>
+        public double[][] Distances(SortedSet<T>[] input, ref SortedSet<T>[][] decision)
+        {
+            return Distances(input, ref decision, new double[input.Length][]);
+        }
+
+        /// <summary>
+        /// Predicts a class label vector for each input vector, returning a
+        /// numerical score measuring the strength of association of the input vector
+        /// to each of the possible classes.
+        /// </summary>
+        /// <param name="input">A set of input vectors.</param>
+        /// <param name="decision">The class labels associated with each input
+        /// vector, as predicted by the classifier. If passed as null, the classifier
+        /// will create a new array.</param>
+        /// <param name="result">An array where the distances will be stored,
+        ///   avoiding unnecessary memory allocations.</param>
+        public double[][] Distances(SortedSet<T>[] input, ref SortedSet<T>[][] decision, double[][] result)
+        {
+            var r = Distances(input, ref decision);
+            r.CopyTo(result);
+            return r;
+        }
+
+        /// <summary>
+        /// Applies the transformation to an input, producing an associated output.
+        /// </summary>
+        /// <param name="input">The input data to which the transformation should be applied.</param>
+        /// <returns>
+        /// The output generated by applying this transformation to the given input.
+        /// </returns>
+        public SortedSet<T>[] Transform(SortedSet<T> input)
+        {
+            return Decide(input);
+        }
+
+        /// <summary>
+        /// Applies the transformation to an input, producing an associated output.
+        /// </summary>
+        /// <param name="input">The input data to which the transformation should be applied.</param>
+        /// <returns>
+        /// The output generated by applying this transformation to the given input.
+        /// </returns>
+        public SortedSet<T>[][] Transform(SortedSet<T>[] input)
+        {
+            return Decide(input);
+        }
+
+        /// <summary>
+        /// Applies the transformation to an input, producing an associated output.
+        /// </summary>
+        /// <param name="input">The input data to which the transformation should be applied.</param>
+        /// <param name="result">An array where the distances will be stored,
+        ///   avoiding unnecessary memory allocations.</param>
+        /// <returns>
+        /// The output generated by applying this transformation to the given input.
+        /// </returns>
+        public SortedSet<T>[][] Transform(SortedSet<T>[] input, SortedSet<T>[][] result)
+        {
+            return Decide(input);
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// Predicts a class label vector for the given input vector, returning a
+        /// numerical score measuring the strength of association of the input vector
+        /// to each of the possible classes.
+        /// </summary>
+        /// <param name="input">A set of input vectors.</param>
+        /// <param name="decision">The class labels associated with each input
+        /// vector, as predicted by the classifier. If passed as null, the classifier
+        /// will create a new array.</param>
+        public double[] Distances(T[] input, ref T[][] decision)
+        {
+            SortedSet<T>[] d = null;
+            var r = Distances(new SortedSet<T>(input), ref d);
+            decision = d.Apply(x => x.ToArray());
+            return r;
+        }
+
+        /// <summary>
+        /// Predicts a class label vector for the given input vector, returning a
+        /// numerical score measuring the strength of association of the input vector
+        /// to each of the possible classes.
+        /// </summary>
+        /// <param name="input">A set of input vectors.</param>
+        /// <param name="decision">The class labels associated with each input
+        /// vector, as predicted by the classifier. If passed as null, the classifier
+        /// will create a new array.</param>
+        /// <param name="result">An array where the distances will be stored,
+        ///   avoiding unnecessary memory allocations.</param>
+        public double[] Distances(T[] input, ref T[][] decision, double[] result)
+        {
+            SortedSet<T>[] d = null;
+            var r = Distances(new SortedSet<T>(input), ref d);
+            decision = d.Apply(x => x.ToArray());
+            Array.Copy(r, result, r.Length);
+            return r;
+        }
+
+        /// <summary>
+        /// Predicts a class label vector for each input vector, returning a
+        /// numerical score measuring the strength of association of the input vector
+        /// to each of the possible classes.
+        /// </summary>
+        /// <param name="input">A set of input vectors.</param>
+        /// <param name="decision">The class labels associated with each input
+        /// vector, as predicted by the classifier. If passed as null, the classifier
+        /// will create a new array.</param>
+        public double[][] Distances(T[][] input, ref T[][][] decision)
+        {
+            return Distances(input, ref decision, new double[input.Length][]);
+        }
+
+        /// <summary>
+        /// Predicts a class label vector for each input vector, returning a
+        /// numerical score measuring the strength of association of the input vector
+        /// to each of the possible classes.
+        /// </summary>
+        /// <param name="input">A set of input vectors.</param>
+        /// <param name="decision">The class labels associated with each input
+        /// vector, as predicted by the classifier. If passed as null, the classifier
+        /// will create a new array.</param>
+        /// <param name="result">An array where the distances will be stored,
+        ///   avoiding unnecessary memory allocations.</param>
+        public double[][] Distances(T[][] input, ref T[][][] decision, double[][] result)
+        {
+            for (int i = 0; i < input.Length; i++)
+                result[i] = Distances(input[i], ref decision[i]);
+            return result;
+        }
+
+        /// <summary>
+        /// Computes a class-label decision for a given <paramref name="input" />.
+        /// </summary>
+        /// <param name="input">The input vector that should be classified into
+        /// one of the <see cref="ITransform.NumberOfOutputs" /> possible classes.</param>
+        /// <returns>
+        /// A class-label that best described <paramref name="input" /> according
+        /// to this classifier.
+        /// </returns>
+        public T[][] Decide(T[] input)
+        {
+            return Decide(new SortedSet<T>(input)).Apply(x => x.ToArray());
+        }
+
+        /// <summary>
+        /// Computes a class-label decision for a given <paramref name="input" />.
+        /// </summary>
+        /// <param name="input">The input vector that should be classified into
+        /// one of the <see cref="ITransform.NumberOfOutputs" /> possible classes.</param>
+        /// <returns>
+        /// A class-label that best described <paramref name="input" /> according
+        /// to this classifier.
+        /// </returns>
+        public T[][][] Decide(T[][] input)
+        {
+            return Decide(input, new T[input.Length][][]);
+        }
+
+        /// <summary>
+        /// Computes a class-label decision for a given <paramref name="input" />.
+        /// </summary>
+        /// <param name="input">The input vector that should be classified into
+        /// one of the <see cref="ITransform.NumberOfOutputs" /> possible classes.</param>
+        /// <param name="result">An array where the distances will be stored,
+        ///   avoiding unnecessary memory allocations.</param>
+        /// <returns>
+        /// A class-label that best described <paramref name="input" /> according
+        /// to this classifier.
+        /// </returns>
+        public T[][][] Decide(T[][] input, T[][][] result)
+        {
+            for (int i = 0; i < input.Length; i++)
+                result[i] = Decide(input[i]);
+            return result;
+        }
+
+        /// <summary>
+        /// Applies the transformation to an input, producing an associated output.
+        /// </summary>
+        /// <param name="input">The input data to which the transformation should be applied.</param>
+        /// <returns>
+        /// The output generated by applying this transformation to the given input.
+        /// </returns>
+        public T[][] Transform(T[] input)
+        {
+            return Decide(input);
+        }
+
+        /// <summary>
+        /// Applies the transformation to an input, producing an associated output.
+        /// </summary>
+        /// <param name="input">The input data to which the transformation should be applied.</param>
+        /// <returns>
+        /// The output generated by applying this transformation to the given input.
+        /// </returns>
+        public T[][][] Transform(T[][] input)
+        {
+            return Decide(input);
+        }
+
+        /// <summary>
+        /// Applies the transformation to an input, producing an associated output.
+        /// </summary>
+        /// <param name="input">The input data to which the transformation should be applied.</param>
+        /// <param name="result"></param>
+        /// <returns>
+        /// The output generated by applying this transformation to the given input.
+        /// </returns>
+        public T[][][] Transform(T[][] input, T[][][] result)
+        {
+            return Decide(input, result);
+        }
+
+
+    }
+}
