3734dbc28ae0199e099d423403ceb9ca911af367
Cesar Souza <cesarsouza@gmail.com>
2016-10-04 22:02:09 +0200

    Small change.

--- Unit Tests/Accord.Tests.Statistics/Testing/TwoSamples/TwoSampleKolmogorovSmirnovTestTest.cs
+++ Unit Tests/Accord.Tests.Statistics/Testing/TwoSamples/TwoSampleKolmogorovSmirnovTestTest.cs
@@ -32,23 +32,6 @@ namespace Accord.Tests.Statistics
     public class TwoSampleKolmogorovSmirnovTestTest
     {
 
-
-        private TestContext testContextInstance;
-
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-
-
         [Test]
         public void TwoSampleKolmogorovSmirnovTestConstructorTest()
         {
