9a690c3cb9353a6585d039efc44e7bc7594a3251
Cesar Souza <cesarsouza@gmail.com>
2017-02-02 18:53:45 +0100

    GH-396: Using Parallel.For with LinearCoordinateDescent
    Updating the main framework random number generator, such that:
     - Its now possible to fix the seed of each executing thread independently of others;
     - Setting the main random seed should now make sure that new random generators created from existing threads will also be initialized with the random seed, except if their thread-specific random seed has been manually overwritten by the user.
    
    Adding unit tests.

--- Sources/Accord.Math/Random/Generator.cs
+++ Sources/Accord.Math/Random/Generator.cs
@@ -51,44 +51,139 @@ namespace Accord.Math.Random
     public static class Generator
     {
 
-        // Random generator used to seed other generators. It is used
-        // to prevent generators that have been created in short spans
-        // to be initialized with the same seed.
-        private static Random source = new Random();
-        private static readonly object sourceLock = new Object();
+        // Random generator used to seed other generators. It is used to prevent generators 
+        // that have been created in short time spans to be initialized with the same seed.
+        private static Random sourceRandom = new Random();
+        private static readonly object sourceRandomLock = new Object();
 
-        private static int? seed;
-        private static readonly object seedLock = new Object();
-
-
-        private static readonly ThreadLocal<Random> random = new ThreadLocal<Random>(create);
+        private static int? sourceSeed;
+        private static long sourceLastUpdateTicks;
+        private static readonly object sourceSeedLock = new Object();
 
 
-        private static Random create()
+        [ThreadStatic]
+        private static long threadLastUpdateTicks;
+
+        [ThreadStatic]
+        private static bool threadOverriden;
+
+        [ThreadStatic]
+        private static int? threadSeed;
+
+        [ThreadStatic]
+        private static Random threadRandom;
+
+
+
+        private static int GetRandomSeed()
         {
             // We initialize new Random objects using the next value from a global 
             // static shared random generator in order to avoid creating many random 
             // objects with the random seed. This guarantees reproducibility but does
             // not compromise the effectiveness of parallel methods that depends on 
             // the generation of true random sequences with different values.
-
-            lock (sourceLock)
+            lock (sourceRandomLock)
             {
-                lock (seedLock)
+                lock (sourceSeedLock)
                 {
-                    if (source == null)
-                        return new Random(Generator.seed.GetValueOrDefault());
-                    return new Random(source.Next());
+                    if (sourceRandom == null)
+                    {
+                        // There is no source random generator. This means we need to initialize the 
+                        // generator for the current thread with a value that is (almost) unpredictable, 
+                        // but still different from threads being initialized at almost the same time.
+
+                        if (Generator.sourceSeed.HasValue)
+                        {
+                            if (Generator.sourceSeed.Value > 0)
+                                return unchecked((int)(13 * Thread.CurrentThread.ManagedThreadId ^ Generator.sourceSeed.Value));
+                            return Generator.sourceSeed.Value;
+                        }
+                        else
+                        {
+                            return unchecked((int)(13 * Thread.CurrentThread.ManagedThreadId ^ DateTime.Now.Ticks));
+                        }
+                    }
+                    else
+                    {
+                        return sourceRandom.Next(); // We have a source random generator
+                    }
                 }
             }
         }
 
         /// <summary>
-        ///   Gets a reference to the random number generator used
-        ///   internally by the Accord.NET classes and methods.
+        ///   Gets the timestamp for when the global random generator
+        ///   was last changed (i.e. after setting <see cref="Seed"/>).
         /// </summary>
         /// 
-        public static Random Random { get { return random.Value; } }
+        public static long LastUpdateTicks
+        {
+            get { return sourceLastUpdateTicks; }
+        }
+
+        /// <summary>
+        ///   Gets the timestamp for when the thread random generator was last 
+        ///   changed (i.e. after creating the first random generator in this 
+        ///   thread context or by setting <see cref="ThreadSeed"/>).
+        /// </summary>
+        /// 
+        public static long ThreadLastUpdateTicks
+        {
+            get { return threadLastUpdateTicks; }
+        }
+
+        /// <summary>
+        ///   Gets or sets the seed for the current thread. Changing
+        ///   this seed will not impact other threads or generators
+        ///   that have already been created from this thread.
+        /// </summary>
+        /// 
+        public static int? ThreadSeed
+        {
+            get { return threadSeed; }
+            set
+            {
+                Generator.threadSeed = value;
+
+                if (value.HasValue)
+                {
+                    Generator.threadOverriden = true;
+                    Generator.threadLastUpdateTicks = DateTime.Now.Ticks;
+                    Generator.threadRandom = (value.HasValue) ? new Random(threadSeed.Value) : new Random();
+                }
+                else
+                {
+                    Generator.threadRandom = null;
+                }
+            }
+        }
+
+        /// <summary>
+        ///   Gets a reference to the random number generator used internally by 
+        ///   the Accord.NET classes and methods. Objects retrieved from this property
+        ///   should not be shared across threads. Instead, call this property from
+        ///   each thread you would like to use a random generator for.
+        /// </summary>
+        /// 
+        public static Random Random
+        {
+            get
+            {
+                if (Generator.threadOverriden)
+                    return threadRandom;
+
+                // No possibility of race condition here since its thread static
+                if (Generator.threadRandom == null || Generator.threadLastUpdateTicks < Generator.sourceLastUpdateTicks)
+                {
+                    Generator.threadSeed = GetRandomSeed();
+                    Generator.threadLastUpdateTicks = Generator.sourceLastUpdateTicks;
+                    Generator.threadRandom = (Generator.threadSeed.HasValue) ? 
+                        new Random(threadSeed.Value) : new Random();
+                }
+
+                return threadRandom;
+            }
+        }
 
         /// <summary>
         ///   Sets a random seed for the framework's main <see cref="Random">internal number 
@@ -101,37 +196,37 @@ namespace Accord.Math.Random
         /// 
         public static int? Seed
         {
-            get { return Generator.seed; }
+            get { return Generator.sourceSeed; }
             set
             {
-                lock (seedLock)
+                lock (sourceSeedLock)
                 {
-                    Generator.seed = value;
+                    Generator.sourceSeed = value;
 
-                    lock (sourceLock)
+                    lock (sourceRandomLock)
                     {
+                        Generator.sourceLastUpdateTicks = DateTime.Now.Ticks;
+
                         if (value.HasValue)
                         {
                             if (value.Value <= 0)
                             {
                                 Trace.WriteLine("All threads will be initialized with the same seed: " + value);
-                                Generator.source = null;
+                                Generator.sourceRandom = null;
                             }
                             else
                             {
                                 Trace.WriteLine("All threads will be initialized with predictable, but random seeds.");
-                                Generator.source = new Random(value.Value);
+                                Generator.sourceRandom = new Random(value.Value);
                             }
                         }
                         else
                         {
                             Trace.WriteLine("All threads will be initialized with unpredictable random seeds.");
-                            int s = (int)(Thread.CurrentThread.ManagedThreadId ^ DateTime.Now.Ticks);
-                            Generator.source = new Random(s);
+                            int s = unchecked((int)(13 * Thread.CurrentThread.ManagedThreadId ^ Generator.sourceLastUpdateTicks));
+                            Generator.sourceRandom = new Random(s);
                         }
                     }
-
-                    Generator.random.Value = create();
                 }
             }
         }
---
+++ Unit Tests/Accord.Tests.Math/Random/GeneratorTest.cs
@@ -0,0 +1,227 @@
+﻿// Accord Unit Tests
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © César Souza, 2009-2017
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+namespace Accord.Tests.Math
+{
+    using System;
+    using Accord.Math.Optimization;
+    using NUnit.Framework;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using Accord.Math;
+    using System.Threading;
+    using System.Collections.Concurrent;
+    using System.Linq;
+    using System.ComponentModel;
+
+    [TestFixture]
+    public class GeneratorTest
+    {
+
+        [Test]
+        public void ParallelTest_zero()
+        {
+            Accord.Math.Random.Generator.Seed = 0;
+            var l = create(100, 10, reset: false);
+            int sameCount = count(l);
+            Assert.IsTrue(sameCount > 50);
+        }
+
+        [Test]
+        public void ParallelTest_less_than_zero()
+        {
+            Accord.Math.Random.Generator.Seed = -1;
+            var l = create(1000, 10, reset: false);
+            int sameCount = count(l);
+            Assert.IsTrue(sameCount > 30);
+        }
+
+        [Test]
+        public void ParallelTest_higher_than_zero()
+        {
+            Accord.Math.Random.Generator.Seed = 1;
+            var l = create(100, 10, reset: false);
+            int sameCount = count(l);
+            Assert.IsTrue(sameCount == 0);
+        }
+
+        [Test]
+        public void ParallelTest_zero_but_reinit()
+        {
+            Accord.Math.Random.Generator.Seed = 0;
+
+            int rows = 100;
+            int cols = 10;
+
+            double[][] l = create(rows, cols, reset: true);
+
+            int sameCount = count(l);
+            Assert.IsTrue(sameCount == 0);
+        }
+
+        [Test]
+        public void ParallelTest_null()
+        {
+            Accord.Math.Random.Generator.Seed = null;
+
+            var l = create(100, 10, reset: false);
+            int sameCount = count(l);
+            Assert.IsTrue(sameCount == 0);
+        }
+
+        [Test]
+        public void consistency()
+        {
+            Accord.Math.Random.Generator.Seed = 0;
+            int[] actual = random(3);
+            int[] expected = new int[] { 1559595546, 1755192844, 1649316166 };
+            var str = actual.ToCSharp();
+            Assert.IsTrue(expected.IsEqual(actual));
+
+            Accord.Math.Random.Generator.Seed = -1;
+            actual = random(3);
+            expected = new int[] { 534011718, 237820880, 1002897798 };
+            str = actual.ToCSharp();
+            Assert.IsTrue(expected.IsEqual(actual));
+
+            Accord.Math.Random.Generator.Seed = 1;
+            actual = random(3);
+            expected = new int[] { 607892308, 1910784178, 911229122 };
+            str = actual.ToCSharp();
+            Assert.IsTrue(expected.IsEqual(actual));
+        }
+
+        [Test]
+        public void thread_initialization()
+        {
+            Accord.Math.Random.Generator.Seed = 0;
+
+            var values = new ConcurrentDictionary<int, List<int>>();
+            var seeds = new ConcurrentDictionary<int, List<int?>>();
+
+            Thread[] t = new Thread[100];
+
+            for (int i = 0; i < 100; i++)
+            {
+                t[i] = new Thread(() =>
+                {
+                    int threadId = Thread.CurrentThread.ManagedThreadId;
+                    var vd = values.GetOrAdd(threadId, new List<int>());
+                    var sd = seeds.GetOrAdd(threadId, new List<int?>());
+
+                    int? before = Accord.Math.Random.Generator.ThreadSeed;
+                    var r = Accord.Math.Random.Generator.Random;
+                    int? after = Accord.Math.Random.Generator.ThreadSeed;
+
+                    int v = r.Next();
+                    vd.Add(v);
+                    sd.Add(before);
+                    sd.Add(after);
+                });
+                t[i].Start();
+            };
+
+            for (int i = 0; i < 100; i++)
+                t[i].Join();
+
+            int[] keys = values.Keys.ToArray().Sorted();
+            Assert.AreEqual(100, keys.Length);
+
+            int? ex = null;
+            for (int i = 0; i < keys.Length; i++)
+            {
+                var l = values[keys[i]];
+                if (ex == null)
+                    ex = l[0];
+                else
+                    Assert.AreEqual(ex.Value, l[0]);
+
+                var s = seeds[keys[i]];
+                // Assert.AreEqual(2, s.Count);
+                Assert.IsNull(s[0]);
+                Assert.AreEqual(0, s[1]);
+            }
+        }
+
+
+
+
+
+
+
+
+        private static int[] random(int n)
+        {
+            var r = Accord.Math.Random.Generator.Random;
+            int[] v = new int[n];
+            for (int i = 0; i < v.Length; i++)
+                v[i] = r.Next();
+            return v;
+        }
+
+        private static int count(IList<double[]> l)
+        {
+            int sameCount = 0;
+            for (int i = 0; i < l.Count; i++)
+            {
+                for (int j = 0; j < l.Count; j++)
+                {
+                    var li = l[i];
+                    var lj = l[j];
+
+                    if (i != j)
+                        if (li.IsEqual(lj, atol: 1e-8))
+                            sameCount++;
+                }
+            }
+
+            return sameCount;
+        }
+
+        private static double[][] create(int rows, int cols, bool reset)
+        {
+            var l = new double[rows][];
+            Thread[] t = new Thread[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                t[i] = new Thread(thread(cols, reset, l));
+                t[i].Start(i);
+            }
+
+            for (int i = 0; i < rows; i++)
+                t[i].Join();
+            return l;
+        }
+
+        private static ParameterizedThreadStart thread(int cols, bool reset, double[][] l)
+        {
+            return (object obj) =>
+            {
+                int j = (int)obj;
+                if (reset)
+                    Accord.Math.Random.Generator.ThreadSeed = j;
+                l[j] = Vector.Random(cols);
+            };
+        }
+    }
+
+}
