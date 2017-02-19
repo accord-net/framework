11e956b2f1bade129afe851c5902e5f999477a1c
Cesar Souza <cesarsouza@gmail.com>
2017-02-19 21:17:21 +0100

    GH-213: Linear SVM with SGD Training Support
    Adding an initial implementation based on the SGD code by Leon Bottou (also LGPL).

---
+++ Unit Tests/Accord.Tests.MachineLearning/VectorMachines/StochasticGradientDescentTest.cs
@@ -0,0 +1,84 @@
+﻿// Accord Unit Tests
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © César Souza, 2009-2017
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+namespace Accord.Tests.MachineLearning
+{
+    using Accord.MachineLearning.VectorMachines;
+    using Accord.MachineLearning.VectorMachines.Learning;
+    using Accord.Math;
+    using Accord.Statistics.Analysis;
+    using Accord.Statistics.Kernels;
+    using NUnit.Framework;
+    using System;
+    using Accord.Statistics.Models.Regression.Linear;
+    using Math.Optimization.Losses;
+
+    [TestFixture]
+    public class StochasticGradientDescentTest
+    {
+
+        [Test]
+        public void LearnTest()
+        {
+
+            double[][] inputs =
+            {
+                new double[] { -1, -1 },
+                new double[] { -1,  1 },
+                new double[] {  1, -1 },
+                new double[] {  1,  1 }
+            };
+
+            int[] xor =
+            {
+                -1,
+                 1,
+                 1,
+                -1
+            };
+
+            var kernel = new Polynomial(2, 0.0);
+
+            double[][] augmented = new double[inputs.Length][];
+            for (int i = 0; i < inputs.Length; i++)
+                augmented[i] = kernel.Transform(inputs[i]);
+
+            // Create the Least Squares Support Vector Machine teacher
+            var learn = new StochasticGradientDescent()
+            {
+                LearningRate = 1e-3
+            };
+
+            // Run the learning algorithm
+            var svm = learn.Learn(augmented, xor);
+
+            bool[] predicted = svm.Decide(augmented);
+            double error = new ZeroOneLoss(xor).Loss(predicted);
+            Assert.AreEqual(0, error);
+
+            int[] output = augmented.Apply(p => Math.Sign(svm.Compute(p)));
+            for (int i = 0; i < output.Length; i++)
+                Assert.AreEqual(System.Math.Sign(xor[i]), System.Math.Sign(output[i]));
+        }
+
+    }
+}
