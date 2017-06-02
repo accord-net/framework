3a5e21e626f926d1e82c9003a1f2d2254fa28cf2
Hashem Zavvari <hashemzawary@gmail.com>
2017-06-02 15:30:06 +0430

    Update ZhangSuenSkeletonization.cs

--- Sources/Accord.Imaging/AForge.Imaging/Filters/Other/ZhangSuenSkeletonization.cs
+++ Sources/Accord.Imaging/AForge.Imaging/Filters/Other/ZhangSuenSkeletonization.cs
@@ -1,12 +1,30 @@
-﻿// HZ Imaging Library
-// The HZ.NET Framework
+// Accord Imaging Library
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © Hashem Zawary, 2017
+// hashemzawary@gmail.com
 // https://www.linkedin.com/in/hashem-zavvari-53b01457
 //
-// Copyright © Hashem Zavvari, 2015-2017
-// hashemzawary at gmail.com
+// Copyright © César Souza, 2009-2017
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
-namespace HZ.Imaging.Filters
+namespace Accord.Imaging.Filters
 {
     using Accord.Imaging;
     using Accord.Imaging.Filters;
