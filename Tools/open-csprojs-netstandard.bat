493a4800a07aad94e9c467ca35362a5cfba5f500
Cesar Souza <cesarsouza@gmail.com>
2017-06-10 19:00:45 +0200

    Fixing compilation in NET35, NET40 and NetStandard

--- Tools/open-csprojs-netstandard.bat
+++ Tools/open-csprojs-netstandard.bat
@@ -5,9 +5,9 @@ set unitest=C:\Projects\Accord.NET\framework\Unit Tests
 set notepad="C:\Program Files (x86)\Notepad++\notepad++.exe"
 echo.
 
-%notepad% "%sources%\Accord.NET.sln"
-%notepad% "%sources%\Accord.NET.targets"
-%notepad% "%unitest%\Accord.Tests.targets"
+%notepad% "%sources%\Accord.NET (NETStandard).sln"
+%notepad% "%sources%\Accord.NET (NETStandard).targets"
+%notepad% "%unitest%\Accord.Tests (NETStandard).targets"
 
 echo %sources%
 for /r "%sources%" %%F in (*Standard^).csproj) do (
