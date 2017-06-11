ff7397b98b86c19aeac477a3d4415461bfa431df
Cesar Souza <cesarsouza@gmail.com>
2017-06-11 19:47:16 +0200

    Preparing 3.5.2-alpha release.

--- Setup/create-release.cmd
+++ Setup/create-release.cmd
@@ -6,38 +6,54 @@
 echo.
 echo =========================================================
 
+if "%1"=="build"   goto build
+if "%1"=="archive" goto archive
+if "%1"=="pack"    goto pack
 
 :: Update version numbers
 cmd /c "update-version.cmd"
+echo.
+echo.
 
+
+:build
 :: Compile sources
 cd ..
 cmd /c "build-all.cmd"
+if %errorlevel% neq 0 exit /b %errorlevel%
 cd Setup
+echo.
+echo.
 
+
+:archive
 :: Build compressed archive
 cd Archiver
 cmd /c "package-framework.cmd"
+if %errorlevel% neq 0 exit /b %errorlevel%
 cmd /c "package-samples.cmd"
+if %errorlevel% neq 0 exit /b %errorlevel%
 cd ..
+echo.
+echo.
 
-echo.
-echo.
 
 :: Build executable installer [disabled since v3.3.0]
 :: cd Installer
 :: cmd /c Build.cmd
 :: cd ..
-:: 
 :: echo.
 :: echo.
 
+
+:pack
 :: Build NuGet packages
 cd NuGet
 cmd /c "create-packages.cmd"
+if %errorlevel% neq 0 exit /b %errorlevel%
 cd ..
+echo.
+echo.
 
-echo.
-echo.
 
 pause
