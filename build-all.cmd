b863bc9173d7bfec1ed053c69090f90c4e917c9d
Cesar Souza <cesarsouza@gmail.com>
2017-07-10 00:26:21 +0200

    Updating build tools.

--- build-all.cmd
+++ build-all.cmd
@@ -30,37 +30,48 @@ echo.
 del /q "Setup\bin\*.log"
 
 echo.
-call:BUILD "Sources\Accord.NET.sln","Debug","x64"
-call:BUILD "Sources\Accord.NET.sln","Debug","Any CPU"
-call:BUILD "Sources\Accord.NET.sln","Mono","Any CPU"
-call:BUILD "Sources\Accord.NET.sln","NET35","x64"
-call:BUILD "Sources\Accord.NET.sln","NET35","Any CPU"
-call:BUILD "Sources\Accord.NET.sln","NET40","x64"
-call:BUILD "Sources\Accord.NET.sln","NET40","Any CPU"
-call:BUILD "Sources\Accord.NET.sln","NET45","x64"
-call:BUILD "Sources\Accord.NET.sln","NET45","Any CPU"
-call:BUILD "Sources\Accord.NET.sln","NET46","x64"
-call:BUILD "Sources\Accord.NET.sln","NET46","Any CPU"
-call:BUILD "Sources\Accord.NET.sln","NET462","x64"
-call:BUILD "Sources\Accord.NET.sln","NET462","Any CPU"
-call:BUILD "Sources\Accord.NET (NETStandard).sln","netstandard2.0","Any CPU"
-call:BUILD "Samples\Samples.sln","Release","x86"
+call:MSBUILD "Sources\Accord.NET.sln", "Debug",   "x64",     "Rebuild"
+call:MSBUILD "Sources\Accord.NET.sln", "Debug",   "Any CPU", "Build"
+call:MSBUILD "Sources\Accord.NET.sln", "mono",    "Any CPU", "Rebuild"
+call:MSBUILD "Sources\Accord.NET.sln", "net35",   "x64",     "Rebuild"
+call:MSBUILD "Sources\Accord.NET.sln", "net35",   "Any CPU", "Build"
+call:MSBUILD "Sources\Accord.NET.sln", "net40",   "x64",     "Rebuild"
+call:MSBUILD "Sources\Accord.NET.sln", "net40",   "Any CPU", "Build"
+call:MSBUILD "Sources\Accord.NET.sln", "net45",   "x64",     "Rebuild"
+call:MSBUILD "Sources\Accord.NET.sln", "net45",   "Any CPU", "Build"
+call:MSBUILD "Sources\Accord.NET.sln", "net46",   "x64",     "Rebuild"
+call:MSBUILD "Sources\Accord.NET.sln", "net46",   "Any CPU", "Build"
+call:MSBUILD "Sources\Accord.NET.sln", "net462",  "x64",     "Rebuild"
+call:MSBUILD "Sources\Accord.NET.sln", "net462",  "Any CPU", "Build"
+call:MSBUILD "Samples\Samples.sln",    "Release", "x86",     "Rebuild"
+
+::: Building netstandard2.0 packages from the command line still doesn't work very well:
+::call:DNBUILD "Sources\Accord.NET (NETStandard).sln","netstandard2.0"
+::call:MSBUILD "Sources\Accord.NET (NETStandard).sln","netstandard2.0","Any CPU",Rebuild
 
 exit /b %ERROR_CODE%
 goto:eof
 
 
+:DNBUILD
+set SOLUTION=%~1
+set CONFIGURATION=%~2
+dotnet build %SOLUTION% --configuration %CONFIGURATION% --no-incremental
+goto:eof
 
-:BUILD
+
+:MSBUILD
 set SOLUTION=%~1
 set CONFIGURATION=%~2
 set PLATFORM=%~3
+set TASK=%~4
 echo.
 echo  - Building %SOLUTION% in %CONFIGURATION% / %PLATFORM% configuration...
-%MSBUILD% /m "%SOLUTION%" /t:Rebuild /p:Configuration=%CONFIGURATION% /p:Platform="%PLATFORM%" /fl /flp:logfile="Setup\bin\Build.%CONFIGURATION%.%PLATFORM%.log";verbosity=normal /consoleloggerparameters:ErrorsOnly;Summary /verbosity:minimal /nologo
+%MSBUILD% /m "%SOLUTION%" /t:%TASK% /property:Prefer32bit=false /p:Configuration=%CONFIGURATION% /p:Platform="%PLATFORM%" /fl /flp:logfile="Setup\bin\Build.%CONFIGURATION%.%PLATFORM%.log";verbosity=normal /consoleloggerparameters:ErrorsOnly;Summary /verbosity:minimal /nologo
 set ERROR_CODE=%errorlevel%
 if %ERROR_CODE% neq 0 (
 	echo Exiting with %ERROR_CODE%
 	exit /b %ERROR_CODE%
 )
 goto:eof
+
