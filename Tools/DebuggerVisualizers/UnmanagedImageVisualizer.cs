fd057402a0c7a469754a69f36d92f36194e47fa9
Cesar Souza <cesarsouza@gmail.com>
2017-08-16 21:37:23 +0200

    Adding the Debugger Visualizers to the main solution; Adjusting output paths according to framework versions; Updating copyright headers and list of contributors;
    Updates GH-773: Debug visualizer updates

--- Tools/DebuggerVisualizers/UnmanagedImageVisualizer.cs
+++ Tools/DebuggerVisualizers/UnmanagedImageVisualizer.cs
@@ -1,9 +1,33 @@
-﻿// AForge debugging visualizers
+﻿// Accord Debugging Visualizers
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © Darren Schroeder, 2017
+// https://github.com/fdncred
+//
+// Copyright © César Souza, 2009-2017
+// cesarsouza at gmail.com
+//
+// AForge debugging visualizers
 // AForge.NET framework
 // http://www.aforgenet.com/framework/
 //
 // Copyright © AForge.NET, 2011
 // contacts@aforgenet.com
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 //
 
 using System.IO;
