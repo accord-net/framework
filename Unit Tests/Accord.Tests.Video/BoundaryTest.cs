88de46a33fa9fde70d41191f6a5d514aedb9f1db
Stig Voss <stig.voss@gmail.com>
2017-08-18 21:44:43 +0200

    Adds EmptyBoundaryTest class to Accord.Tests.Video Adds BoundaryTest class to Accord.Tests.Video

---
+++ Unit Tests/Accord.Tests.Video/BoundaryTest.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Accord.Video;
+using System.IO;
+
+namespace Accord.Tests.Video
+{
+    [TestFixture]
+    public class BoundaryTest
+    {
+        private string _value = "boundary";
+        private Boundary _boundary;
+
+        [SetUp]
+        public void Initialization()
+        {
+            _boundary = new Boundary(_value);
+        }
+
+        [Test]
+        public void HasValueTest()
+        {
+            Assert.IsTrue(_boundary.HasValue);
+        }
+
+        [Test]
+        public void IsCheckedTest()
+        {
+            Assert.IsFalse(_boundary.IsChecked);
+        }
+
+        [Test]
+        public void IsValidTest()
+        {
+            Assert.IsFalse(_boundary.IsValid);
+        }
+
+        [Test]
+        public void LengthTest()
+        {
+            int expected = _value.Length;
+            Assert.AreEqual(expected, _boundary.Length);
+        }
+
+        [Test]
+        public void ContentTest()
+        {
+            string expected = _value;
+            Assert.AreEqual(expected, _boundary.Content);
+        }
+        
+        [Test]
+        public void FixMalformedBoundaryTest()
+        {
+            string expected = "--" + _value;
+            byte[] buffer = Encoding.ASCII.GetBytes(expected);
+
+            MJPEGStreamParser parser = new MJPEGStreamParser(_boundary, new byte[0]);
+            MemoryStream stream = new MemoryStream(buffer);
+
+            parser.Read(stream);
+
+            _boundary.FixMalformedBoundary(parser);
+            
+            Assert.AreEqual(expected, _boundary.Content);
+        }
+
+        [Test]
+        public void FixMalformedAndCheckedBoundaryTest()
+        {
+            string expected = "--" + _value;
+            byte[] buffer = Encoding.ASCII.GetBytes(expected);
+
+            MJPEGStreamParser parser = new MJPEGStreamParser(_boundary, new byte[0]);
+            MemoryStream stream = new MemoryStream(buffer);
+
+            parser.Read(stream);
+
+            _boundary.FixMalformedBoundary(parser);
+
+            Assert.IsTrue(_boundary.IsChecked);
+        }
+
+        [Test]
+        public void FixMalformedAndUnchangedBoundaryTest()
+        {
+            string expected = _value;
+            byte[] buffer = Encoding.ASCII.GetBytes(expected);
+
+            MJPEGStreamParser parser = new MJPEGStreamParser(_boundary, new byte[0]);
+            MemoryStream stream = new MemoryStream(buffer);
+
+            parser.Read(stream);
+
+            _boundary.FixMalformedBoundary(parser);
+
+            Assert.IsTrue(_boundary.IsChecked);
+        }
+
+        [Test]
+        public void FixMalformedAndValidBoundaryTest()
+        {
+            string expected = "--" + _value;
+            byte[] buffer = Encoding.ASCII.GetBytes(expected);
+
+            MJPEGStreamParser parser = new MJPEGStreamParser(_boundary, new byte[0]);
+            MemoryStream stream = new MemoryStream(buffer);
+
+            parser.Read(stream);
+
+            _boundary.FixMalformedBoundary(parser);
+
+            Assert.IsTrue(_boundary.IsValid);
+        }
+
+        [Test]
+        public void FixMalformedBoundaryAndLengthTest()
+        {
+            string expected = "--" + _value;
+            byte[] buffer = Encoding.ASCII.GetBytes(expected);
+
+            MJPEGStreamParser parser = new MJPEGStreamParser(_boundary, new byte[0]);
+            MemoryStream stream = new MemoryStream(buffer);
+
+            parser.Read(stream);
+
+            _boundary.FixMalformedBoundary(parser);
+
+            Assert.AreEqual(_boundary.Length, expected.Length);
+        }
+
+        [Test]
+        public void PrependTest()
+        {
+            string expected = "--" + _value;
+
+            _boundary.Prepend('-');
+            _boundary.Prepend('-');
+
+            Assert.AreEqual(expected, _boundary.Content);
+        }
+    }
+}
---
+++ Unit Tests/Accord.Tests.Video/EmptyBoundaryTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Accord.Video;
+
+namespace Accord.Tests.Video
+{
+    [TestFixture]
+    public class EmptyBoundaryTest
+    {
+        private Boundary _boundary;
+
+        [SetUp]
+        public void Initialization()
+        {
+            _boundary = new Boundary();
+        }
+
+        [Test]
+        public void HasValueTest()
+        {
+            Assert.IsFalse(_boundary.HasValue);
+        }
+
+        [Test]
+        public void IsCheckedTest()
+        {
+            Assert.IsFalse(_boundary.IsChecked);
+        }
+
+        [Test]
+        public void IsValidTest()
+        {
+            Assert.IsTrue(_boundary.IsValid);
+        }
+
+        [Test]
+        public void LengthTest()
+        {
+            int expected = 0;
+            Assert.AreEqual(expected, _boundary.Length);
+        }
+
+        [Test]
+        public void ContentTest()
+        {
+            string expected = string.Empty;
+            Assert.AreEqual(expected, _boundary.Content);
+        }
+    }
+}
