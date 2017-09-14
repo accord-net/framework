ad727162cc600f357cacc7f7bdde2b2079bd7ea3
Cesar Souza <cesarsouza@gmail.com>
2017-09-14 22:18:09 +0200

    GH-473: Add an Example for IParallel.ParallelOptions Property

--- Sources/Accord.Core/MachineLearning/IParallel.cs
+++ Sources/Accord.Core/MachineLearning/IParallel.cs
@@ -33,8 +33,24 @@ namespace Accord.MachineLearning // TODO: Move those classes to the root Accord 
     public interface IParallel : ISupportsCancellation
     {
         /// <summary>
-        ///   Gets or sets the parallelization options for this algorithm.
+        ///   Gets or sets the parallelization options for this algorithm. It can
+        ///   be used to control the maximum number of cores that should be used
+        ///   during the algorithm's execution.
         /// </summary>
+        /// 
+        /// <remarks>
+        ///   The <see cref="IParallel"/> interface is implemented by most machine learning
+        ///   algorithms in the framework, and it is most common use is to allow the user to
+        ///   tune how many cores should be used by a multi-threaded learning algorithm.
+        /// </remarks>
+        /// 
+        /// <example>
+        /// <para>
+        ///   In the following example, we will be using the <see cref="ParallelOptions"/> property to limit 
+        ///   the maximum degree of parallelism of a support vector machine learning algorithm to be 1, meaning
+        ///   the algorithm will be running in a single thread:</para>
+        ///   <code source="Unit Tests\Accord.Tests.MachineLearning\VectorMachines\MulticlassSupportVectorLearningTest.cs" region="doc_learn_ldcd" />
+        /// </example>
         /// 
         ParallelOptions ParallelOptions { get; set; }
     }
