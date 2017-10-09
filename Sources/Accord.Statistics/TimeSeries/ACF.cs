cd4365829f8de6136ebb0dcf65bb70cbaa2dde17
Liang Xie <xie1978@hotmail.com>
2017-10-09 00:22:32 -0700

    Updated ACF function based on CR. Build Pass under VS 2017

--- Sources/Accord.Statistics/TimeSeries/ACF.cs
+++ Sources/Accord.Statistics/TimeSeries/ACF.cs
@@ -4,6 +4,8 @@
 //
 // Copyright © César Souza, 2009-2017
 // cesarsouza at gmail.com
+// Copyright Liang Xie 2017
+// xie1978 at hotmail dot com
 //
 //    This library is free software; you can redistribute it and/or
 //    modify it under the terms of the GNU Lesser General Public
@@ -20,68 +22,91 @@
 //    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
-
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Accord.Math    
-
-public static double[] ACF(double[] vector, int nlag)
+namespace Accord.Statistics.TimeSeries
 {
-	
-    int nTime = vector.Length;   
-    if (nlag<1)
+    using Accord.Math;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Numerics;
+    
+
+    public static class stattools
     {
-       nlag = nTime;
+        /// <summary>
+        ///   Calculates the auto correlation function using Wiener–Khinchin theorem.
+        /// </summary>
+        /// 
+        /// <param name="vector">
+        ///   A vector of observations whose AutoCorrelation Function will be calculated. It is assumed that
+        ///   the vector is a 1-D array of type double
+        /// </param>
+        /// <param name="nlag">
+        ///   Number of lags to return autocorrelation for. Integer.
+        /// </param>
+        /// 
+        /// <returns>
+        ///   Returns a vector of type double giving the autocorrelation function upto given lags.
+        /// </returns>
+        /// 
+        public static double[] ACF(double[] vector, int nlag)
+        {
+
+            int nTime = vector.Length;
+            if (nlag < 1)
+            {
+                nlag = nTime;
+            }
+            if (nTime <= 1)
+            {
+                //throw new System.ArgumentException("Vector length should be >=2", "vector");
+                double[] acf1 = new double[] { 1.0 };
+                return acf1;
+            }            
+            // padding the length to be the power of 2 to facilitate FFT speed.
+            int newLength = Convert.ToInt32(Math.Pow(2, Math.Ceiling(Math.Log(nTime, 2))));
+
+            // define and calculate FFT
+            double VecMean = vector.Mean();
+            Complex[] Frf = new Complex[newLength];
+            for (int k = 0; k < newLength; k++)
+            {
+                if (k < nTime)
+                {
+                    Frf[k] = new Complex(vector[k] - VecMean, 0);
+                }
+                else
+                {
+                    Frf[k] = 0;
+                }
+            }
+            Accord.Math.Transforms.FourierTransform2.FFT(Frf, Accord.Math.FourierTransform.Direction.Forward);
+
+            // calculate inverse(backward) FFT of ( Frf*Conjugate(Frf) )
+            Complex[] iFTFTj = new Complex[Frf.Length];
+            for (int k = 0; k < Frf.Length; k++)
+            {
+                Complex FrfConj = Complex.Conjugate(Frf[k]);
+                double RealPart = Frf[k].Real * FrfConj.Real - Frf[k].Imaginary * FrfConj.Imaginary;
+                double ImaginaryPart = Frf[k].Real * FrfConj.Imaginary + Frf[k].Imaginary * FrfConj.Real;
+                iFTFTj[k] = new Complex(RealPart, ImaginaryPart);
+            }
+            Accord.Math.Transforms.FourierTransform2.FFT(iFTFTj, Accord.Math.FourierTransform.Direction.Backward);
+
+            // calculate ACF, normalized against the first item
+            double[] acf = new double[nTime];
+            double normalizer = 1.0;
+            int newlag = nTime < nlag ? nTime : nlag;
+            for (int k = 0; k < newlag; k++)
+            {
+                acf[k] = iFTFTj[k].Real / (nTime * normalizer);
+                if (k == 0)
+                {
+                    normalizer = acf[0];
+                    acf[0] = 1.0;
+                }
+            }
+            return acf;
+        }
     }
-    if  (nTime<=1)
-    {
-       //throw new System.ArgumentException("Vector length should be >=2", "vector");
-       double[] acf1 = new double[] { 1.0 };
-       return acf1;
-    }
-    // padding the length to be the power of 2 to facilitate FFT speed.
-    int newLength = Convert.ToInt32(Math.Pow(2, Math.Ceiling( Math.Log(nTime, 2) ) )) ;
-  
-    // define and calculate FFT
-    double VecMean = vector.Mean();
-    Complex[] Frf = new Complex[newLength];
-    for (int k = 0; k< newLength; k++)
-    {
-         if (k < nTime)
-         {
-            Frf[k] = new Complex(Convert.ToDouble(vector[k] - VecMean), 0);
-         }
-         else
-         {  
-            Frf[k]=0;
-         }
-     }
-     Accord.Math.Transforms.FourierTransform2.FFT(Frf, Accord.Math.FourierTransform.Direction.Forward);
-           
-     // calculate inverse(backward) FFT of ( Frf*Conjugate(Frf) )
-     Complex[] iFTFTj = new Complex[Frf.Length];
-     for (int k=0; k < Frf.Length; k++)
-     {
-        Complex FrfConj = Complex.Conjugate(Frf[k]);
-        double RealPart = Frf[k].Real*FrfConj.Real - Frf[k].Imaginary*FrfConj.Imaginary;
-        double ImaginaryPart = Frf[k].Real*FrfConj.Imaginary + Frf[k].Imaginary*FrfConj.Real;
-        iFTFTj[k] = new Complex(RealPart, ImaginaryPart);
-     }                    
-     Accord.Math.Transforms.FourierTransform2.FFT(iFTFTj, Accord.Math.FourierTransform.Direction.Backward);
-  
-     // calculate ACF, normalized against the first item
-     double[] acf = new double[nTime];
-     double normalizer = 1.0;        
-     int newlag = nTime<nlag ? nTime : nlag;
-     for (int k=0; k < newlag; k++)
-     {
-         acf[k] = iFTFTj[k].Real / (nTime * normalizer);
-         if (k==0)
-         {
-             normalizer = acf[0];
-             acf[0]=1.0;
-         }              
-     }          
-     return acf;
 }
