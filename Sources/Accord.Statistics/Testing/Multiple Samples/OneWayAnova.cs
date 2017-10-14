63239e3842e2bbc7aef1da10086c792c8e6c9d95
Cesar Souza <cesarsouza@gmail.com>
2017-10-14 23:06:51 +0200

    Adding missing example images for Accord.Neuro documentation and fixing image paths.

--- Sources/Accord.Statistics/Testing/Multiple Samples/OneWayAnova.cs
+++ Sources/Accord.Statistics/Testing/Multiple Samples/OneWayAnova.cs
@@ -95,7 +95,7 @@ namespace Accord.Statistics.Testing
     /// 
     /// <para>Result in:</para>
     /// 
-    /// <img src="../images/one-way-anova.png"/>
+    /// <img src="..\images\one-way-anova.png"/>
     /// 
     /// <para>
     ///  The p-level for the analysis is about 0.002, meaning the test is
