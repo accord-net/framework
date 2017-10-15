9e54d7997487f2ccb0b1532d06d42875edae5f77
Cesar Souza <cesarsouza@gmail.com>
2017-10-15 23:48:06 +0200

    Trying to fix Travis' & AppVeyor's build. Seems that Mono's native stack overflows were being caused by a bug in mono triggered by the presence of a interface specification in both a generic class and its generic argument.

--- Sources/Accord.Core/Collections/BinaryNode.cs
+++ Sources/Accord.Core/Collections/BinaryNode.cs
@@ -33,7 +33,7 @@ namespace Accord.Collections
     /// <typeparam name="TNode">The class type for the nodes of the tree.</typeparam>
     /// 
     [Serializable]
-    public class BinaryNode<TNode> : IEquatable<TNode> // TODO: Try to remove IEquatable
+    public class BinaryNode<TNode> : IEquatable<TNode>, ITreeNode<TNode> // TODO: Try to remove IEquatable
         where TNode : BinaryNode<TNode>
     {
         /// <summary>
