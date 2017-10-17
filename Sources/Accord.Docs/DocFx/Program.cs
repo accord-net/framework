7cfd958f34fbaaf8688bfa2ac79fd1aa057bd0e9
Cesar Souza <cesarsouza@gmail.com>
2017-10-17 22:07:01 +0200

    Preparing the room for GH-790: Upgrade documentation generation system to DocFX

---
+++ Sources/Accord.Docs/DocFx/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Diagnostics;
+
+namespace Accord.DocFx
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Environment.CurrentDirectory = Path.Combine(Environment.CurrentDirectory, @"..\..");
+            string docfx = Path.GetFullPath(@"..\..\..\..\Sources\packages\docfx.console.2.25.1\build\..\tools\docfx.exe");
+
+            Console.WriteLine("Working dir: " + Environment.CurrentDirectory);
+            Console.WriteLine("DocFx path : " + docfx);
+
+            Console.ReadKey();
+
+            Process.Start(@"..\..\..\Sources\packages\docfx.console.2.25.1\build\..\tools\docfx.exe", 
+                "--serve").WaitForExit();
+        }
+    }
+}
