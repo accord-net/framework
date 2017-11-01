45943bb3f905c00ad4de369ed622f76df3410233
Cesar Souza <cesarsouza@gmail.com>
2017-11-01 21:03:15 +0100

    Updating VideoSourcePlayer to offer a more memory-efficient API.

--- Sources/Accord.Controls.Imaging/AForge/VideoSourcePlayer.Designer.cs
+++ Sources/Accord.Controls.Imaging/AForge/VideoSourcePlayer.Designer.cs
@@ -1,8 +1,26 @@
-﻿// AForge Controls Library
-// AForge.NET framework
+﻿// Accord Control Library
+// The Accord.NET Framework
+// http://accord-framework.net
+//
+// Copyright © César Souza, 2009-2017
+// cesarsouza at gmail.com
 //
 // Copyright © Andrew Kirillov, 2005-2008
 // andrew.kirillov@gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
 namespace Accord.Controls
@@ -18,13 +36,13 @@ namespace Accord.Controls
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose( bool disposing )
+        protected override void Dispose(bool disposing)
         {
-            if ( disposing && ( components != null ) )
+            if (disposing && (components != null))
             {
-                components.Dispose( );
+                components.Dispose();
             }
-            base.Dispose( disposing );
+            base.Dispose(disposing);
         }
 
         #region Component Designer generated code
@@ -33,15 +51,15 @@ namespace Accord.Controls
         /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent( )
+        private void InitializeComponent()
         {
-            this.SuspendLayout( );
+            this.SuspendLayout();
             // 
             // VideoSourcePlayer
             // 
-            this.Paint += new System.Windows.Forms.PaintEventHandler( this.VideoSourcePlayer_Paint );
-            this.ParentChanged += new System.EventHandler( this.VideoSourcePlayer_ParentChanged );
-            this.ResumeLayout( false );
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.VideoSourcePlayer_Paint);
+            this.ParentChanged += new System.EventHandler(this.VideoSourcePlayer_ParentChanged);
+            this.ResumeLayout(false);
 
         }
 
