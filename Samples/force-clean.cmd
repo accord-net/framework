370ec47682df24a80d01c416a5c1c3f1e7a95f33
Cesar Souza <cesarsouza@gmail.com>
2017-11-11 00:34:25 +0100

    Updating cleaning scripts to clean also Binaries and packages folders; Minor updates to sample application configurations;

--- Samples/force-clean.cmd
+++ Samples/force-clean.cmd
@@ -1,2 +1,4 @@
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S Binaries') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S packages') DO RMDIR /S /Q "%%G"
