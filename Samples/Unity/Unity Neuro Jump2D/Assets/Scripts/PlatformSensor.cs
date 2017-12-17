7b518a76fb72eedc992a252223add03cc5c1e9d2
MaxAl <maxaltenhofer@gmx.at>
2017-12-17 15:16:15 +0100

    GH-1050 Improved text coloring in Animat; Added unity sample for simple jump learning with activation network;

---
+++ Samples/Unity/Unity Neuro Jump2D/Assets/Scripts/PlatformSensor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSensor : MonoBehaviour {
+
+    public Collider FirstCollider;
+    public Collider SecondCollider;
+
+    private bool _isFirstObjectToggle = true;
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.name.Contains("Player")) {
+            return;
+        }
+
+        if (_isFirstObjectToggle) {
+            _isFirstObjectToggle = false;
+            FirstCollider = other;
+        } else {
+            SecondCollider = FirstCollider;
+            FirstCollider = other;
+        }
+    }
+}
