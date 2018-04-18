bcab96c397881ec960034777bebc8edff8776bb7
quantumdot <thackray@rutgers.edu>
2018-04-18 18:17:54 -0400

    added implementation of IAMVideoProAmp interface and associated methods on VideoCaptureDevice
    see issue #303 for details, basically following the formula here: https://github.com/accord-net/framework/issues/303#issuecomment-382520745

---
+++ Sources/Accord.Video.DirectShow/Internals/IAMVideoProcAmp.cs
@@ -0,0 +1,80 @@
+﻿// AForge Direct Show Library
+// AForge.NET framework
+// http://www.aforgenet.com/framework/
+//
+// Copyright © AForge.NET, 2009-2013
+// contacts@aforgenet.com
+//
+
+namespace Accord.Video.DirectShow.Internals
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// The IAMVideoProcAmp interface controls video camera settings such as brightness, contrast, hue,
+    /// or saturation. To obtain this interface, cast the MediaSource.
+    /// </summary>
+    [ComImport, System.Security.SuppressUnmanagedCodeSecurity,
+    Guid("C6E13360-30AC-11D0-A18C-00A0C9118956"),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IAMVideoProcAmp
+    {
+        /// <summary>
+        /// Get the range and default value of a camera property.
+        /// </summary>
+        ///
+        /// <param name="Property">The property.</param>
+        /// <param name="pMin">The min value.</param>
+        /// <param name="pMax">The max value.</param>
+        /// <param name="pSteppingDelta">The step size.</param>
+        /// <param name="pDefault">The deafult value. </param>
+        /// <param name="pCapsFlags">Shows if it can be controlled automatically and/or manually.</param>
+        ///
+        /// <returns>Error code.</returns>
+        ///
+        [PreserveSig]
+        int GetRange(
+            [In] VideoProcAmpProperty Property,
+            [Out] out int pMin,
+            [Out] out int pMax,
+            [Out] out int pSteppingDelta,
+            [Out] out int pDefault,
+            [Out] out VideoProcAmpFlags pCapsFlags
+            );
+
+        /// <summary>
+        /// Set a specified property on the camera.
+        /// </summary>
+        ///
+        /// <param name="Property">The property to set.</param>
+        /// <param name="lValue">The new value of the property.</param>
+        /// <param name="Flags">The auto or manual setting.</param>
+        ///
+        /// <returns>Error code.</returns>
+        ///
+        [PreserveSig]
+        int Set(
+            [In] VideoProcAmpProperty Property,
+            [In] int lValue,
+            [In] VideoProcAmpFlags Flags
+            );
+
+        /// <summary>
+        /// Get the current setting of a camera property.
+        /// </summary>
+        ///
+        /// <param name="Property">The property to retrieve.</param>
+        /// <param name="lValue">The current value of the property.</param>
+        /// <param name="Flags">Is it manual or automatic?</param>
+        ///
+        /// <returns>Error code.</returns>
+        ///
+        [PreserveSig]
+        int Get(
+            [In] VideoProcAmpProperty Property,
+            [Out] out int lValue,
+            [Out] out VideoProcAmpFlags Flags
+            );
+    }
+}
--- Sources/Accord.Video.DirectShow/VideoCaptureDevice.cs
+++ Sources/Accord.Video.DirectShow/VideoCaptureDevice.cs
@@ -1019,6 +1019,158 @@ namespace Accord.Video.DirectShow
 
             return ret;
         }
+        /// <summary>
+        /// Sets a specified property on the video signal adjustments.
+        /// </summary>
+        /// 
+        /// <param name="property">Specifies the property to set.</param>
+        /// <param name="value">Specifies the new value of the property.</param>
+        /// <param name="controlFlags">Specifies the desired control setting.</param>
+        /// 
+        /// <returns>Returns true on success or false otherwise.</returns>
+        /// 
+        /// <exception cref="ArgumentException">Video source is not specified - device moniker is not set.</exception>
+        /// <exception cref="ApplicationException">Failed creating device object for moniker.</exception>
+        /// <exception cref="NotSupportedException">The video source does not support camera control.</exception>
+        /// 
+        public bool SetVideoProcAmpProperty(VideoProcAmpProperty property, int value, VideoProcAmpFlags controlFlags)
+        {
+            bool ret = true;
+
+            // check if source was set
+            if (String.IsNullOrEmpty(deviceMoniker))
+                throw new ArgumentException("Video source is not specified.");
+
+            lock (sync)
+            {
+                object tempSourceObject = null;
+
+                // create source device's object
+                try
+                {
+                    tempSourceObject = FilterInfo.CreateFilter(deviceMoniker);
+                }
+                catch
+                {
+                    throw new ApplicationException("Failed creating device object for moniker.");
+                }
+
+                if (!(tempSourceObject is IAMVideoProcAmp))
+                    throw new NotSupportedException("The video source does not support video signal adjustments.");
+
+                IAMVideoProcAmp pVideoProcAmp = (IAMVideoProcAmp)tempSourceObject;
+                int hr = pVideoProcAmp.Set(property, value, controlFlags);
+
+                ret = (hr >= 0);
+
+                Marshal.ReleaseComObject(tempSourceObject);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Gets the current setting of a video signal adjustment property.
+        /// </summary>
+        /// 
+        /// <param name="property">Specifies the property to retrieve.</param>
+        /// <param name="value">Receives the value of the property.</param>
+        /// <param name="controlFlags">Receives the value indicating whether the setting is controlled manually or automatically</param>
+        /// 
+        /// <returns>Returns true on sucee or false otherwise.</returns>
+        /// 
+        /// <exception cref="ArgumentException">Video source is not specified - device moniker is not set.</exception>
+        /// <exception cref="ApplicationException">Failed creating device object for moniker.</exception>
+        /// <exception cref="NotSupportedException">The video source does not support camera control.</exception>
+        /// 
+        public bool GetVideoProcAmpProperty(VideoProcAmpProperty property, out int value, out VideoProcAmpFlags controlFlags)
+        {
+            bool ret = true;
+
+            // check if source was set
+            if (String.IsNullOrEmpty(deviceMoniker))
+                throw new ArgumentException("Video source is not specified.");
+
+            lock (sync)
+            {
+                object tempSourceObject = null;
+
+                // create source device's object
+                try
+                {
+                    tempSourceObject = FilterInfo.CreateFilter(deviceMoniker);
+                }
+                catch
+                {
+                    throw new ApplicationException("Failed creating device object for moniker.");
+                }
+
+                if (!(tempSourceObject is IAMVideoProcAmp))
+                    throw new NotSupportedException("The video source does not support video signal adjustments.");
+
+                IAMVideoProcAmp pVideoProcAmp = (IAMVideoProcAmp)tempSourceObject;
+                int hr = pVideoProcAmp.Get(property, out value, out controlFlags);
+
+                ret = (hr >= 0);
+
+                Marshal.ReleaseComObject(tempSourceObject);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Gets the range and default value of a specified video stream property.
+        /// </summary>
+        /// 
+        /// <param name="property">Specifies the property to query.</param>
+        /// <param name="minValue">Receives the minimum value of the property.</param>
+        /// <param name="maxValue">Receives the maximum value of the property.</param>
+        /// <param name="stepSize">Receives the step size for the property.</param>
+        /// <param name="defaultValue">Receives the default value of the property.</param>
+        /// <param name="controlFlags">Receives a member of the <see cref="CameraControlFlags"/> enumeration, indicating whether the property is controlled automatically or manually.</param>
+        /// 
+        /// <returns>Returns true on sucee or false otherwise.</returns>
+        /// 
+        /// <exception cref="ArgumentException">Video source is not specified - device moniker is not set.</exception>
+        /// <exception cref="ApplicationException">Failed creating device object for moniker.</exception>
+        /// <exception cref="NotSupportedException">The video source does not support camera control.</exception>
+        /// 
+        public bool GetVideoProcAmpRange(VideoProcAmpProperty property, out int minValue, out int maxValue, out int stepSize, out int defaultValue, out VideoProcAmpFlags controlFlags)
+        {
+            bool ret = true;
+
+            // check if source was set
+            if (String.IsNullOrEmpty(deviceMoniker))
+                throw new ArgumentException("Video source is not specified.");
+
+            lock (sync)
+            {
+                object tempSourceObject = null;
+
+                // create source device's object
+                try
+                {
+                    tempSourceObject = FilterInfo.CreateFilter(deviceMoniker);
+                }
+                catch
+                {
+                    throw new ApplicationException("Failed creating device object for moniker.");
+                }
+
+                if (!(tempSourceObject is IAMVideoProcAmp))
+                    throw new NotSupportedException("The video source does not support video signal adjustments.");
+
+                IAMVideoProcAmp pVideoProcAmp = (IAMVideoProcAmp)tempSourceObject;
+                int hr = pVideoProcAmp.GetRange(property, out minValue, out maxValue, out stepSize, out defaultValue, out controlFlags);
+
+                ret = (hr >= 0);
+
+                Marshal.ReleaseComObject(tempSourceObject);
+            }
+
+            return ret;
+        }
 
         /// <summary>
         /// Worker thread.
