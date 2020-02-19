57de8d2c6fcbe2857851949d96fe7c0a6d0e6829
pinelibg <pinelibg@outlook.jp>
2020-02-20 02:53:22 +0900

    Invert dimensions when autoTranspose is True

--- Sources/Accord.Math/IO/Mat/MatNode.cs
+++ Sources/Accord.Math/IO/Mat/MatNode.cs
@@ -118,6 +118,9 @@ namespace Accord.IO
                 Array src = Value as Array;
                 Array dst = Array.CreateInstance(targetType, dimensions);
 
+                if (matReader.Transpose)
+                    dst = dst.Transpose();
+
                 foreach (int[] idx in src.GetIndices())
                     dst.SetValue(Convert.ChangeType(src.GetValue(idx), targetType), idx);
 
